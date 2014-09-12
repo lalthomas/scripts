218d3cd1f68cb4cecd8c40b0dd536fb08c67af6e
Lal Thomas <rapid@RapidValues-Mac-mini-2.local>
2014-09-12 11:25:31 +0530

    do folder changes

---
+++ work/@schedule-to-todo.sh
@@ -0,0 +1,10 @@
+# Dump the today's scheduled task to todo.txt
+
+BASEDIR=$(dirname $0)
+longdate=$(date "+%Y-%m-%d")
+calendarfilepath="$BASEDIR/calendar.txt"
+todofilepath="$BASEDIR/todo.txt"
+
+# find the string and remove the double of the result returned by grep
+  
+grep $longdate "$calendarfilepath" | tr -d "\n" >> "$todofilepath"
