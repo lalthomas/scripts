288750947f679bdbc41375d050f3a7a965e1e8ca
Lal Thomas <lal.thomas.mail@gmail.com>
2015-08-26 04:31:36 +0530

    add filepaths bash script

---
+++ 20150826-filepaths-bash script.sh
@@ -0,0 +1,32 @@
+# find the OS type for rootPath
+
+case "$OSTYPE" in
+	darwin*) 
+	# OSX
+	export rootPath="/Users/rapid/Dropbox" 		
+	;; 
+	msys*) 
+	# Windows
+	export rootPath="/d/Dropbox"  	
+	;;		
+	cygwin*) 
+	# Windows
+	export rootPath="d:/Dropbox"  	
+	;;		
+	*) echo "unknown: $OSTYPE" ;;
+esac
+
+### todo file paths
+
+export doRootPath="$rootPath/action/20140310-do"
+export doPlannerFile="$doRootPath/planner.md"
+export doTodoFile="$doRootPath/todo.txt"
+export doInvalidFile="$doRootPath/invalid.txt"
+export doJournalPath="$doRootPath/journal.md"
+export doLogPath="$doRootPath/log.txt"
+
+### docs
+
+export docRootPath="$rootPath/docs"	
+export docJournalFile="$docRootPath/$today-journal.md"
+export docYesterdayJournalFile="$docRootPath/$yesterday-journal.md"
