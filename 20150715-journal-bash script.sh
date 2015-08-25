1fccc3106835fb9dff243dad09a0567ba8f96ff5
Lal Thomas <lal.thomas.mail@gmail.com>
2015-08-26 04:30:54 +0530

    cleanup journal creation script

--- 20150715-journal-bash script.sh
+++ 20150715-journal-bash script.sh
@@ -3,45 +3,29 @@
 # Date : 2015-07-15
 # © Lal Thomas (lal.thomas.mail@gmail.com)
 
-extension=".md"
-journalfilename=$today-$dayOfWeeK" journal"$extension
-journalfilepath="$rootpath/docs/$journalfilename"
-yesterdayjournalfilename=$yesterday-$dayofWeekYesterday" journal"$extension
-yesterdayjournalfilepath="$rootpath/docs/$yesterdayjournalfilename"
 
 # journal utlity functions
 
 createJournalFile(){
-
-	local todofolder="$1"
-	local copydir="$2"
-	local journalname="$3"
-	local todofilepath="$todofolder/todo.txt"
-	local journalpath="$copydir/$journalname"
-	local plannerfilepath="$todofolder/planner.md"
-	local sectionplannerfilepath="$todofolder/journal.md"	
-
-	#printf $rootpath
-
-
+		
 	# check if file exists or not
-	if [ ! -f "$copydir"/"$journalname" ];then
+	if [ ! -f "$docJournalFile" ];then
 
-	  createMarkdownHeading "1" "$today" "$journalpath"	  
-	  createMarkdownHeading "2" "Scheduled Tasks" "$journalpath"   
+	  createMarkdownHeading "1" "$today $dayOfWeeK Journal" "$docJournalFile"
+	  createMarkdownParagraph "normal" "*Date of Creation* : $longdate" "$docJournalFile"	  
+	  createMarkdownHeading "2" "Scheduled Tasks" "$docJournalFile"   
 	  
 	  # Dump the today's scheduled task to todo.txt and extra line breaks	  
-	  sed -n -e 's/'$longdate'/* &/p'<"$todofilepath" >>"$journalpath"
-	  printf "\n">>"$journalpath"
+	  sed -n -e 's/'$longdate'/* &/p'<"$doTodoFile" >>"$docJournalFile"
+	  printf "\n">>"$docJournalFile"
 
 	  # Read input file into a string variable. 
 	  # Thanks : http://stackoverflow.com/a/2789399/2182047  
-	  copyfilecontent=$(cat "$sectionplannerfilepath")
+	  copyfilecontent=$(cat "$doJournalPath")
 	  #copy contents to journal file
-	  printf "$copyfilecontent" >>"$journalpath"  
+	  printf "$copyfilecontent" >>"$docJournalFile"  
 	  # add two blank line
-	  printf "\n\n" >>"$journalpath"	  	   	  
-	  mv "$journalpath" "$copydir"/"$journalname"
+	  printf "\n\n" >>"$docJournalFile"	  
 
 	fi
 
@@ -50,11 +34,11 @@ createJournalFile(){
 	case "$OSTYPE" in
 	darwin*) 
 		# OSX		
-		open "$copydir"/"$journalname"		
+		open "$docJournalFile"		
 		;; 
 	msys*)
 		# Windows
-		start "" "$copydir"/"$journalname"
+		start "" "$docJournalFile"
 		;; 		
 	*) 
 		echo "unknown: $OSTYPE" 
@@ -66,31 +50,22 @@ createJournalFile(){
 
 addMyDoneItemsToJournal(){
 
-createMarkdownHeading "2" "Done Tasks" "$journalfilepath"
-t listall "x $longdate" | sed -n -e 's/[0-9][0-9][0-9] x [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ \* /p'>>"$journalfilepath"
+	createMarkdownHeading "2" "Done Tasks" "$docJournalFile"
+	t listall "x $longdate" | sed -n -e 's/[0-9][0-9][0-9] x [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ \* /p'>>"$docJournalFile"
 }
 
 addMyDoneItemsToYesterdayJournal(){
 
 	## //TODO : improve
-	createMarkdownHeading "2" "Done Tasks" "$yesterdayjournalfilename"
-	$1 listall "x $longyesterday" | sed -n -e 's/[0-9][0-9][0-9] x [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ \* /p'>>"$yesterdayjournalfilename"
+	createMarkdownHeading "2" "Done Tasks" "$docYesterdayJournalFile"
+	$1 listall "x $longyesterday" | sed -n -e 's/[0-9][0-9][0-9] x [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ \* /p'>>"$docYesterdayJournalFile"
 
 }
 
-alias adddoneitemstomyjournal="addMyDoneItemsToJournal"
-alias adddoneitemstoyesterdaymyjournal="addMyDoneItemsToYesterdayJournal"
-
-alias createmyjournal="createJournalFile '$rootpath/do' '$rootpath/docs' '$journalfilename'"
-alias createjournal="createmyjournal && createworkjournal && createdevjournal"
-
-AddTimeToFile(){
-
-	local tag=$1
-	local filename=$2
-	echo "* $tag : $(date +'%T')" >> "$filename"
-
+log(){
+	
+	echo "$now $@">> $doLogPath
 }
 
-alias addcheckintimetoworkjournal="AddTimeToFile 'Checkin Time' '$workjournalfilepath'"
-alias addcheckouttimetoworkjournal="AddTimeToFile 'Checkout Time' '$workjournalfilepath'"
+alias addcheckintimetolog="log 'checkin' "
+alias addcheckouttimetolog="log 'checkout'"
