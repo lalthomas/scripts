bc3b78a5bc2edf465b10f0e5557f08a7409c3185
Lal Thomas <lal.thomas.mail@gmail.com>
2015-08-26 05:46:50 +0530

    update the do bash testing script

--- 20150825-do-bash script-testing script.sh
+++ 20150825-do-bash script-testing script.sh
@@ -1,1 +1,26 @@
-echo $currentScriptFolder
+echo currentScriptFolder: $currentScriptFolder
+echo rootPath : $rootPath
+echo doRootPath : $doRootPath
+# ------------------
+# t alias test start
+t
+# t alias test stop
+# ------------------
+# todo alias test start
+todo
+# todo alias test stop
+# ------------------
+# todoarchive alias test start
+todoarchive
+# todoarchive alias  test stop
+# ------------------
+# addreport alias test start
+addreport
+# addreport alias test stop
+# addtodobirdseyereport alias test start
+addtodobirdseyereport
+# addtodobirdseyereport alias test stop
+# scheduletododailytasks alias test start
+scheduletododailytasks
+# scheduletododailytasks alias test stop
+
