07ea289dde21a0d9e81d47520632048d26363f13
Lal Thomas <lal.thomas.mail@gmail.com>
2015-10-30 18:44:28 +0530

    update filelist.txt stub holder name

--- 20140222-copy files from a list of files-dos batch script.bat
+++ 20140222-copy files from a list of files-dos batch script.bat
@@ -3,7 +3,7 @@ set dst_folder="F:\ted"
 
 REM  Loop through the filelist
 
-for /f "delims=" %%a in (filelist-10.txt) do (
+for /f "delims=" %%a in (filelist.txt) do (
 xcopy /S /I %%a %dst_folder%
 )
 pause
