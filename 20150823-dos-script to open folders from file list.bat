e39c814b1874bd81dceb924c4e6e7c36030fe261
Lal Thomas <lal.thomas.mail@gmail.com>
2015-11-04 07:21:58 +0530

    format and set the echo off

--- 20150823-dos-script to open folders from file list.bat
+++ 20150823-dos-script to open folders from file list.bat
@@ -1,17 +1,13 @@
-@echo ON
+@echo OFF
 setlocal
-
-REM This will start explorer with a folder
-REM start explorer  "D:\Dropbox\action\20150716-contacts"
-
 cd "%~dp1"
-
 for /f "tokens=*" %%a in ( %~n1%~x1 ) do (  
   start explorer "%%a"
 )
-
 endlocal
 
+REM This will start explorer with a folder
+REM start explorer "D:\Dropbox\action\20150716-contacts"
 REM This will start explorer++ with a list of folders
 REM "D:\PortableApps.com\PortableApps\Explorer++Portable\Explorer++Portable.exe" "D:\Dropbox\action\20150716-contacts" "D:\Dropbox\action\20140915-playlist"
 
