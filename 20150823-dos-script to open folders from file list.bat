21608042b6df33e4016249f285817ddcb0ebc6b6
Lal Thomas <lal.thomas.mail@gmail.com>
2015-11-04 07:20:06 +0530

    fix the issue of not able to run from shortcut by passing argument

--- 20150823-dos-script to open folders from file list.bat
+++ 20150823-dos-script to open folders from file list.bat
@@ -1,14 +1,17 @@
-@echo OFF
+@echo ON
+setlocal
+
 REM This will start explorer with a folder
 REM start explorer  "D:\Dropbox\action\20150716-contacts"
+
+cd "%~dp1"
+
+for /f "tokens=*" %%a in ( %~n1%~x1 ) do (  
+  start explorer "%%a"
+)
+
+endlocal
 
 REM This will start explorer++ with a list of folders
 REM "D:\PortableApps.com\PortableApps\Explorer++Portable\Explorer++Portable.exe" "D:\Dropbox\action\20150716-contacts" "D:\Dropbox\action\20140915-playlist"
 
-filelist=%1
-
-for /f "tokens=*" %%a in ( %%filelist ) do (  
-  start explorer "%%a"
-)
-
-pause
