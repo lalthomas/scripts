28c25558d1c468d945947779c3a9f1e71d96be44
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-15 10:57:27 +0530

    remove absolute references to tools with relative reference

--- 20131027-dev-beautify source code-dos batch script.bat
+++ 20131027-dev-beautify source code-dos batch script.bat
@@ -1,7 +1,11 @@
 REM Author Lal Thomas (lal.thomas.mail@gmail.com)
-@echo ON
-setlocal
+@echo OFF
 
+REM get the script folder path
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+
+setlocal
 if /i %~x1 == .c ( goto CGROUP )
 if /i %~x1 == .cpp ( goto CGROUP )
 if /i %~x1 == .cs ( goto CGROUP )
@@ -10,45 +14,46 @@ if /i %~x1 == .html ( goto  HTML )
 if /i %~x1 == .htm ( goto HTML )
 if /i %~x1 == .xml ( goto  XML )
 if /i %~x1 == .css ( goto CSS)
-goto End
+goto END
 
 :CGROUP
 REM C,C++,C#,Java
-set path=%PATH%;E:\Devel\Mis\astyle
+set path=%PATH%;"%scriptFolderPath%\tools\astyle\"
+pause
 REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd "%~p1"
 cls
 call astyle --brackets=break --max-instatement-indent=0 --indent-brackets --indent-labels --indent-namespaces --indent-cases --indent-switches --indent-classes --convert-tabs %1
 IF %ERRORLEVEL% EQU 0 (del %1.orig) ELSE (pause echo Lexical Error in Program )
-goto End
+goto END
 
 :HTML
 REM HTML
-copy %1 E:\Temp\ArchBty.html
-set path=%PATH%;E:\Devel\Mis\htb
+copy %1 %scriptFolderPath%\temp.html
+set path=%PATH%;"%scriptFolderPath%\tools\htb"
 REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd "%~p1"
 cls
-call htb /ablns4 E:\Temp\ArchBty.html %1
-goto End
+call htb /ablns4 %scriptFolderPath%\temp.html %1
+goto END
 
 :XML
 REM HTML
-copy %1 E:\Temp\ArchBty.xml
-set path=%PATH%;E:\Devel\Mis\xml-indent
+copy %1 "%scriptFolderPath%\temp.xml"
+set path=%PATH%;"%scriptFolderPath%\tools\xml-indent"
 REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd "%~p1"
 cls
-call xmlindent -o %1 E:\Temp\ArchBty.xml 
-goto End
+call xmlindent -o %1 %scriptFolderPath%\temp.xml 
+goto END
 
 :CSS
 REM CSS
-copy %1 E:\Temp\ArchBty.css
-set path=%PATH%;E:\Devel\Mis\css-tidy
+copy %1 "%scriptFolderPath%\temp.css"
+set path=%PATH%;"%scriptFolderPath%\tools\css-tidy"
 @echo OFF
 REM The following two line are Npp Hack for not changing the current path
 %~d1
@@ -66,10 +71,10 @@ SET _OptValue= "--template=low"
 IF /I "%_Opt%" == "1" ( SET _OptValue= --template=low)
 IF /I "%_Opt%" == "2" ( SET _OptValue= --template=medium)
 IF /I "%_Opt%" == "3" ( SET _OptValue= --template=high)
-call csstidy E:\Temp\ArchBty.css --timestamp=false --allow_html_in_templates=false --compress_colors=false --compress_font=false --lowercase_s=true --preserve_css=true --remove_last_;=false --remove_bslash=false --sort_properties=true --sort_selectors=false --case_properties=0 %_OptValue% %1
-IF %ERRORLEVEL% NOT EQU 0 (copy E:\Temp\ArchBty.css %1 )
-goto End
+call csstidy "%scriptFolderPath%\temp.css" --timestamp=false --allow_html_in_templates=false --compress_colors=false --compress_font=false --lowercase_s=true --preserve_css=true --remove_last_;=false --remove_bslash=false --sort_properties=true --sort_selectors=false --case_properties=0 %_OptValue% %1
+IF %ERRORLEVEL% NOT EQU 0 (copy "%scriptFolderPath%\temp.css" %1 )
+goto END
 
-:End
+:END
 REM pause
 endlocal
