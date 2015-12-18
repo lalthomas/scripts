9a2a11aee945f2d64bcb0a3c400c7e7291fd816b
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-18 22:15:26 +0530

    combine the path of the compiles to variables
    - remove absolute paths

--- 20111220-compile programs-dos batch script.bat
+++ 20111220-compile programs-dos batch script.bat
@@ -1,6 +1,16 @@
 @echo OFF
 setlocal
 
+
+REM Program variables
+set CCompilerPath="C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\gcc.exe"
+set CppCompilterPath="C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\g++.exe"
+set JavaCompilerPath="C:\Program Files\Java\jdk1.7.0_51\bin\javac.exe"
+
+REM get the script folder path
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+
 if /i %~x1 == .c ( goto C )
 if /i %~x1 == .cpp ( goto CPP )
 if /i %~x1 == .cs ( goto CS)
@@ -9,19 +19,18 @@ goto End
 
 REM C
 :C
-set path=%PATH%;"C:\Program Files (x86)\Dev-Cpp\MinGW64\bin"
 REM The following two line are Npp Hack for not changing the current path
 %~d1
-cd "%~p1"
-IF EXIST "%~n1".exe (del "%~n1".exe) ELSE (echo %1 is being processed first time)
-gcc "%1"
+cd %~p1
+IF EXIST %~n1.exe (del %~n1.exe) ELSE (echo %1 is being processed first time)
+%CCompilerPath% %1
 IF %ERRORLEVEL% EQU 0 (goto CSuccess ) ELSE (goto CFailure)
 :CFailure
 echo Program terminated with Compilation Errors
 pause
 goto End
 :CSuccess
-ren a.exe "%~n1".exe
+ren a.exe %~n1.exe
 goto End
 
 
@@ -29,19 +38,17 @@ REM Cpp
 :CPP
 @echo OFF
 setlocal
-set path=%PATH%;"C:\Program Files (x86)\Dev-Cpp\MinGW64\bin"
 REM The following two line are Npp Hack for not changing the current path
 %~d1
-cd "%~p1"
-IF EXIST "%~n1".exe (del "%~n1".exe) ELSE (echo %1 is being processed first time)
-g++ "%1"
+cd %~p1
+IF EXIST %~n1.exe (del %~n1.exe) ELSE (echo %1 is being processed first time)
+%CppCompilterPath% %1
 IF %ERRORLEVEL% EQU 0 (goto CppSuccess ) ELSE (goto CppFailure)
 :CppFailure
 echo Program terminated with Compilation Errors
-pause
 goto End
 :CppSuccess
-ren a.exe "%~n1".exe
+ren a.exe %~n1.exe
 goto End
 
 
@@ -56,7 +63,7 @@ goto EOS
 :Args
 echo Parsing Arguments...
 REM Creating another temporary batch file
-type E:\Devel\Mis\csc.txt "%CD%\CmdOpt.txt" E:\Devel\Mis\space.txt>>temp.bat 
+type E:\Devel\Mis\csc.txt "%CD%\CmdOpt.txt" %scriptFolderPath%\space.txt>>temp.bat 
 echo %1 >>temp.bat
 echo IF %%ERRORLEVEL%% EQU 0 ( echo "Successfully Compiled..." ) >>temp.bat
 cls
@@ -70,12 +77,11 @@ REM Java
 :JAVA
 @echo OFF
 setlocal
-set path=%PATH%;D:\Program Files\Java\jdk1.6.0_23\bin
 REM The following two line are Npp Hack for not changing the current path
 %~d1
-cd "%~p1"
+cd %~p1
 ::javac %1
-javac %1
+%JavaCompilerPath% %1
 goto End
 
 :End
