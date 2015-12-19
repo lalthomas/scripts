631bc5c00930846b6dd60997c959c644cdc74517
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-20 01:06:38 +0530

    add support for php running on iis webserver

--- 20100827-run dev programs-dos batch script.bat
+++ 20100827-run dev programs-dos batch script.bat
@@ -5,9 +5,12 @@ REM get the script folder path
 set scriptFolderPathFull=%~dp0%
 set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 
+set IISPATH="C:\inetpub\wwwroot\site"
+
 if /i %~x1 == .html ( goto HTML)
 if /i %~x1 == .java ( goto JAVA )
 if /i %~x1 == .class ( goto JAVA )
+if /i %~x1 == .php ( goto PHP )
 REM Execute Exe,Bat
 set path=%PATH%;%CD%
 %~d1
@@ -33,6 +36,54 @@ cd "%~p1"
 call java "%~n1"
 goto END
 
+REM PHP
+:PHP
+@echo OFF
+setlocal
+%~d1
+set OrginalPath=%CD%
+set ParentDir=%~p1
+REM this reduce the 
+call :getParentFolderName
+
+REM create temporary batch file for running as adminstrator
+if not exist temp.bat (
+echo C:>temp.bat
+echo cd %IISPATH%>>temp.bat
+echo mklink /d "%ParentDir%" "%OrginalPath%">>temp.bat
+echo del temp.bat>>temp.bat
+)
+
+REM change the path
+C:
+cd %IISPATH%
+
+if not exist "%ParentDir%" ( 
+echo "%OrginalPath%\temp.bat"
+REM invoke the adminstrator dialog box
+Powershell -Command "& {Start-Process" \"%OrginalPath%\\temp.bat\"-verb RunAs}"
+)
+
+call "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "http://localhost/site/%ParentDir%/%~n1%~x1"
+del "%OrginalPath%\temp.bat"
+goto END
+
+
+:getParentFolderName
+REM thanks : http://stackoverflow.com/questions/2396003/get-parent-directory-name-for-a-particular-file-using-dos-batch-scripting
+set ParentDir=%ParentDir: =:%
+set ParentDir=%ParentDir:\= %
+call :getparentdir %ParentDir%
+set ParentDir=%ParentDir::= %
+
+
+:getparentdir
+if "%~1" EQU "" goto END
+set ParentDir=%~1
+shift
+goto :getparentdir
+
 :END
-pause
 endlocal
+
+
