af7243505fd69e9b5659962caecc65cab4ed2563
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-20 01:06:03 +0530

    remove debug helper codes

--- 20121123-rename file with parent folder name-dos batch script.bat
+++ 20121123-rename file with parent folder name-dos batch script.bat
@@ -9,33 +9,28 @@ setlocal
 REM set ParentDir=%~p1
 set ParentDir=%CD%
 set ParentDir=%~p1
-REM echo %ParentDir%
 
 REM Replaces every space with colon
 set ParentDir=%ParentDir: =:%
-REM echo %ParentDir%
 
 REM Replaces every back slash with space
 set ParentDir=%ParentDir:\= %
-REM echo %ParentDir%
 
 REM Calls a routine that calculates the last folder
 call :getparentdir %ParentDir%
 
 REM Replaces every colon with space
 set ParentDir=%ParentDir::= %
-REM echo ParentDir is %ParentDir%
 
 REM End of Script (Partial 1) 
-REM ****************************************************
-
+REM *************************************
 REM Code here since there is call command
 ::REN %1 "[%ParentDir%]-%~n1%~x1"
 REN %1 "%ParentDir%%~x1"
 REM pause
 endlocal
 
-REM ****************************************************
+REM *********************************************************
 REM Script to get the last folder in the file path (Partial 2)
 REM Routine that returns the last word of the string
 :getparentdir
