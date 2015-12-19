3b4f70324d37cc6990bb0dbb6b36cd5168bfafc2
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-19 19:59:21 +0530

    add batch beautify update for folder

--- 20131027-dev-beautify source code-dos batch script.bat
+++ 20131027-dev-beautify source code-dos batch script.bat
@@ -1,11 +1,22 @@
 @echo OFF
 REM Author Lal Thomas (lal.thomas.mail@gmail.com)
-
 REM get the script folder path
 set scriptFolderPathFull=%~dp0%
 set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 
 setlocal
+REM Thanks you http://stackoverflow.com/a/2541820
+IF [%~x1] == [] ( 
+IF EXIST %1 ( CALL:FOLDER "%CD%" )
+) ELSE ( 
+IF EXIST %1 ( CALL:MAP %1 )
+)
+REM premature exit to define functions beneath
+EXIT /B %ERRORLEVEL%
+
+REM Subroutine
+:MAP
+REM echo %1
 if /i %~x1 == .c ( goto CGROUP )
 if /i %~x1 == .cpp ( goto CGROUP )
 if /i %~x1 == .cs ( goto CGROUP )
@@ -14,8 +25,9 @@ if /i %~x1 == .html ( goto  HTML )
 if /i %~x1 == .htm ( goto HTML )
 if /i %~x1 == .xml ( goto  XML )
 if /i %~x1 == .css ( goto CSS)
-goto END
+EXIT /b 0
 
+REM Section
 :CGROUP
 REM C,C++,C#,Java
 set path=%PATH%;"%scriptFolderPath%\tools\astyle\"
@@ -25,8 +37,9 @@ cd "%~p1"
 cls
 call astyle --style=gnu %1
 IF %ERRORLEVEL% EQU 0 (del %1.orig) ELSE (pause echo Lexical Error in Program )
-goto END
+EXIT /b 0
 
+REM Section
 :HTML
 REM HTML
 copy %1 %scriptFolderPath%\temp.html
@@ -36,8 +49,9 @@ REM The following two line are Npp Hack for not changing the current path
 cd "%~p1"
 cls
 call htb /ablns4 %scriptFolderPath%\temp.html %1
-goto END
+EXIT /b 0
 
+REM Section
 :XML
 REM HTML
 copy %1 "%scriptFolderPath%\temp.xml"
@@ -47,7 +61,7 @@ REM The following two line are Npp Hack for not changing the current path
 cd "%~p1"
 cls
 call xmlindent -o %1 %scriptFolderPath%\temp.xml 
-goto END
+EXIT /b 0
 
 :CSS
 REM CSS
@@ -72,8 +86,15 @@ IF /I "%_Opt%" == "2" ( SET _OptValue= --template=medium)
 IF /I "%_Opt%" == "3" ( SET _OptValue= --template=high)
 call csstidy "%scriptFolderPath%\temp.css" --timestamp=false --allow_html_in_templates=false --compress_colors=false --compress_font=false --lowercase_s=true --preserve_css=true --remove_last_;=false --remove_bslash=false --sort_properties=true --sort_selectors=false --case_properties=0 %_OptValue% %1
 IF %ERRORLEVEL% NOT EQU 0 (copy "%scriptFolderPath%\temp.css" %1 )
-goto END
+pause
+EXIT /b 0
 
-:END
+:FOLDER
+SET /p _Opt="Are you sure to beautify all files on the folder(y/n)" 
+IF "%_Opt%" == "n" ( goto :EOF)
+echo Batch Processing folder... : %1 
+for %%a in ("%CD%"\*.*) do ( CALL:MAP "%%a" )
+EXIT /b 0
+
 REM pause
 endlocal
