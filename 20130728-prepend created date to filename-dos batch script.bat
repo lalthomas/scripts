b0ec071b79c73c7a017b768830f32ea7d9d58578
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-22 19:35:22 +0530

    rename to more meaningful name

---
+++ 20130728-prepend created date to filename-dos batch script.bat
@@ -0,0 +1,30 @@
+@echo OFF
+REM ****************************************************
+setlocal
+set path=%PATH%;%~dp1
+%~d1
+cd %~p1
+REM Script to get the attributes of file
+for /f "skip=5 tokens=1-8 delims=/: " %%a in ('dir /tC %1') do (
+     set mon=%%a      
+     set day=%%b
+     set yyyy=%%c
+     set hh=%%d
+     set min=%%e
+     set filename=%%h
+     goto label
+)
+:label
+REM %mon% %day% %yyyy% %hh% %min% %filename%
+REM Replaces all empty space
+set mon=%mon: =%
+set time=%yyyy%%mon%%day%
+echo %time%
+REM pause
+REM goto :EOF
+REM ****************************************************
+
+REN "%~n1%~x1" "%time%-%~n1%~x1"
+
+endlocal
+REM pause
