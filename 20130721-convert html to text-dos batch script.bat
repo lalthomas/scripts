716786f3ded4dbd8faf67a41c81ffa913cff17b4
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-22 18:22:37 +0530

    update the tools to relative path

--- 20130721-convert html to text-dos batch script.bat
+++ 20130721-convert html to text-dos batch script.bat
@@ -1,12 +1,17 @@
 @echo OFF
 setlocal
+
+REM get the script folder path
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+
 set path=%PATH%;E:\Devel\Mis\Html2text
-del E:\Temp\ARCH.txt
-copy %1 "E:\Temp\Arch.htm"
-call HTML2TXT E:\Temp\Arch.htm @HTML2TXT.INI -AB-I-h--o:A 
-copy "E:\Temp\ARCH.txt" "%~d1%~p1%~n1.txt"
-REM ::pause
-REM goto end
-REM :end
+
+del "%temp%\%~n1.%~p1"
+copy %1 "%temp%"
+
+call "%scriptFolderPath%\tools\html2text\html2txt.exe" "%temp%\%~n1.%~p1" @html2txt.ini -AB-I-h--o:A 
+copy "%temp%\%~n1.txt" "%~d1%~p1%~n1.txt"
+
 endlocal
 REM pause
