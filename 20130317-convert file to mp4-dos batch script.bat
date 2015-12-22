293f18fa785d47536caf2cd218ef0b2d2c5aa31c
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-22 18:06:35 +0530

    move the documentation to readme file

--- 20130317-convert file to mp4-dos batch script.bat
+++ 20130317-convert file to mp4-dos batch script.bat
@@ -1,19 +1,16 @@
 setlocal
 @echo OFF
 set path=%PATH%;%CD%
+REM get the script folder path
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+
 REM Working (best size)
 REM Batch willn't run on mp4 file
 IF /I "%~x1" == ".mp4" ( goto END)
 
-REM config for big files
-:: call E:\Devel\Mis\Video\ffmpeg.exe -i %1 -qmin 2 -qmax 5 -ar 22050 "%~n1.mp4"
+call "%scriptFolderPath%\tools\ffmpeg bundle\ffmpeg.exe" -i %1 -c:v libx264 -crf 23 "%~p1\%~n1.mp4"
 
-REM config for small files
-REM To improve the video quality, you can use a lower CRF value,
-REM e.g. anything down to 18. To get a smaller file, use a higher CRF, 
-REM but note that this will degrade quality.
-REM To improve the audio quality, use a higher quality value. For FAAC, 100 is default.
-call E:\Devel\Mis\Video\ffmpeg.exe -i %1 -c:v libx264 -crf 23 "%~n1.mp4"
 ::pause
 :END
 endlocal
