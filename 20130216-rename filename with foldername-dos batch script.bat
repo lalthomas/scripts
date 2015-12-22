de361b3287f6f6984c5e66705ccace00a156cac1
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-22 19:40:16 +0530

    remove time stamp addition from the script
    - this not needed

--- 20130216-rename filename with foldername-dos batch script.bat
+++ 20130216-rename filename with foldername-dos batch script.bat
@@ -1,20 +1,4 @@
 @echo OFF
-REM ****************************************************
-REM Script to get the attributes of file
-for /f "skip=5 tokens=1-8 delims=/: " %%a in ('dir /tc %1') do (
-     set mon=%%a      
-     set day=%%b
-     set yyyy=%%c
-     set hh=%%d
-     set min=%%e
-     set filename=%%h
-     goto label
-)
-:label
-REM Replaces all empty space
-set mon=%mon: =%
-echo %mon% %day% %yyyy% %hh% %min% %filename%
-REM ****************************************************
 set path=%PATH%;%~dp1
 %~d1
 cd %~p1
@@ -44,18 +28,17 @@ REM ****************************************************
 
 
 :MAKE_FOLDER 
-set timestamp=%yyyy%%mon%%day%
 
 set /a s=2
 :CHECKPOINT
-	if EXIST "%timestamp%-%ParentDir%-%%~n1 %s%%~x1" (goto MORE)
+	if EXIST "%ParentDir%-%%~n1 %s%%~x1" (goto MORE)
 	goto CONT
 	:MORE	
 	set /a s=s+1	
 goto CHECKPOINT
 
 :CONT
-REN "%~n1%~x1" "%timestamp%-%ParentDir%-%~n1%~x1"
+REN "%~n1%~x1" "%ParentDir%-%~n1%~x1"
 
 REM pause
 
