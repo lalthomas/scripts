de08014ca7c8bee4419c0a42c2a312100e727165
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-22 18:34:21 +0530

    add windows explorer integration

--- 20120713-copy a file to all sub folders-dos batch script.bat
+++ 20120713-copy a file to all sub folders-dos batch script.bat
@@ -1,4 +1,4 @@
-
-set file=""
+set file=%1
 REM for batch file use %%f else use %f
-for /f "tokens=*" %%f in ('dir . /ad/b') do copy %file% "%%f"
+for /f "tokens=*" %%f in ('dir "%~p1" /ad/b') do copy %file% "%~p1\%%f"
+pause
