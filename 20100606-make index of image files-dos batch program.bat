235fa37436b24e92f9f1772afa4f892791191d8c
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-23 20:12:10 +0530

    made date and time of the file as filename

--- 20100606-make index of image files-dos batch program.bat
+++ 20100606-make index of image files-dos batch program.bat
@@ -1,11 +1,29 @@
 setlocal
 @echo ON
 REM get the script folder path
+
+REM Script to get the attributes of file
+for /f "skip=5 tokens=1-8 delims=/: " %%a in ('dir /tC %1') do (
+     set mon=%%a      
+     set day=%%b
+     set yyyy=%%c
+     set hh=%%d
+     set min=%%e
+     set filename=%%h
+     goto receiveDateTimeString
+)
+:receiveDateTimeString
+set mon=%mon: =%
+set datetime=%yyyy%%mon%%day%-%hh%%min%
+
+REM find script folder 
 set scriptFolderPathFull=%~dp0%
 set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+
 set "headerFilePath=\templates\20151223-image file index template 01.html"
 set "footerFilePath=\templates\20151223-image file index template 02.html"
-set filename=temp.html
+
+set filename=%datetime%-index.html
 
 set path=%PATH%;%~dp1
 %~d1
