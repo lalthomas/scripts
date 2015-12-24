557a7dc079d922098d9cc3208e3cbb02735a6206
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-24 11:36:27 +0530

    fix the issue of line ignored when folder name contains # (hash symbol)

--- 20150823-dos-script to open folders from file list.bat
+++ 20150823-dos-script to open folders from file list.bat
@@ -1,10 +1,8 @@
-@echo ON
+@echo OFF
 setlocal
 cd "%~dp1"
 for /f "tokens=*" %%a in ( %~n1%~x1 ) do (  
-
-  echo."%%a" | findstr /C:"# ">nul && ( echo %%a skipped ) || ( start explorer "%%a" )
- 
+  echo %%a | findstr /r "^\#" >nul && ( echo . ) || ( start explorer "%%a" )    
 )
-pause
+REM pause
 endlocal
