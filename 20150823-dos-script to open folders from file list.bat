e5bf34de318f5fe2125c36861c4647eda0e6f58e
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-24 10:25:12 +0530

    remove the documentation part to readme

--- 20150823-dos-script to open folders from file list.bat
+++ 20150823-dos-script to open folders from file list.bat
@@ -6,8 +6,3 @@ for /f "tokens=*" %%a in ( %~n1%~x1 ) do (
 )
 endlocal
 
-REM This will start explorer with a folder
-REM start explorer "D:\Dropbox\action\20150716-contacts"
-REM This will start explorer++ with a list of folders
-REM "D:\PortableApps.com\PortableApps\Explorer++Portable\Explorer++Portable.exe" "D:\Dropbox\action\20150716-contacts" "D:\Dropbox\action\20140915-playlist"
-
