23f9aa71a9c45c6c0a01e82e2effcd1b4c29a974
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-24 23:20:05 +0530

    fix the issue of not opening folders if the argument file name contains spaces

--- 20150823-open folders from file list-dos script batch script.bat
+++ 20150823-open folders from file list-dos script batch script.bat
@@ -1,8 +1,8 @@
 @echo OFF
 setlocal
 cd "%~dp1"
-for /f "tokens=*" %%a in ( %~n1%~x1 ) do (  
+set "filename=%~dpnx1"
+for /f "tokens=*" %%a in ( 'type "%filename%"' ) do (  
   echo %%a | findstr /r "^\#" >nul && ( echo . ) || ( start explorer "%%a" )    
 )
-REM pause
 endlocal
