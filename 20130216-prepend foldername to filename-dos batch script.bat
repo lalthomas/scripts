095ff14e707463f63725b5fc484bc9991532f91e
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-26 13:25:53 +0530

    receive the drive and folder path of arguments correctly

--- 20130216-prepend foldername to filename-dos batch script.bat
+++ 20130216-prepend foldername to filename-dos batch script.bat
@@ -1,45 +1,16 @@
-@echo OFF
-set path=%PATH%;%~dp1
-%~d1
-cd %~p1
-
-REM ****************************************************
-REM Script to get the last folder in the file path
-REM Credit : http://stackoverflow.com/questions/2396003/get-parent-directory-name-for-a-particular-file-using-dos-batch-scripting
-@echo OFF
-
+@echo ON
 setlocal
-set ParentDir=%~p1
-set ParentDir=%ParentDir: =:%
-set ParentDir=%ParentDir:\= %
-call :getparentdir %ParentDir%
-set ParentDir=%ParentDir::= %
-echo ParentDir is %ParentDir%
-goto :MAKE_FOLDER
+for %%a in ( %* ) do (  
 
-:getparentdir
-if "%~1" EQU "" goto :MAKE_FOLDER
-Set ParentDir=%~1
-shift
-goto :getparentdir
-
-REM End of Script
-REM ****************************************************
-
-
-:MAKE_FOLDER 
-
-set /a s=2
-:CHECKPOINT
-	if EXIST "%ParentDir%-%%~n1 %s%%~x1" (goto MORE)
-	goto CONT
-	:MORE	
-	set /a s=s+1	
-goto CHECKPOINT
-
-:CONT
-REN "%~n1%~x1" "%ParentDir%-%~n1%~x1"
-
-REM pause
-
-
+  echo file : %%~na
+  echo file drive : %%~da
+  echo file folder path : %%~dpa
+  echo .
+  
+  %%~da
+  cd "%%~pa"
+  dir
+  
+)
+endlocal
+pause
