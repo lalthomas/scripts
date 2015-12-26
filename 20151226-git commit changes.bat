0a7e249895f3c07800e7d4820c2c2605b1304e9d
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-26 20:08:27 +0530

    add files

---
+++ 20151226-git commit changes.bat
@@ -0,0 +1,9 @@
+@echo ON
+
+set gitPath="C:\Program Files (x86)\Git\bin\sh.exe"
+
+set /p _commitMessage=Enter the commit message :
+
+%gitPath% --login -i -c "git add ." "%~1" && %gitPath% --login -i -c "git commit -am'%_commitMessage%' " "%~1"
+
+pause
