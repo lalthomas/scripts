8ea309f7955cc388a3f0388e696fd9da47e961c2
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-26 20:10:30 +0530

    update the file names

--- 20151226-git commit changes.bat
+++ 20151226-git commit changes.bat
@@ -4,6 +4,6 @@ set gitPath="C:\Program Files (x86)\Git\bin\sh.exe"
 
 set /p _commitMessage=Enter the commit message :
 
-%gitPath% --login -i -c "git add ." "%~1" && %gitPath% --login -i -c "git commit -am'%_commitMessage%' " "%~1"
+%gitPath% --login -i -c "git add -A" "%~1" && %gitPath% --login -i -c "git commit -am'%_commitMessage%' " "%~1"
 
 pause
