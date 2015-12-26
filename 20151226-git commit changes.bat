53c75effa2e10826401a82fb76450ab7d99398c7
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-26 22:47:38 +0530

    update the script for commit with spaces

--- 20151226-git commit changes.bat
+++ 20151226-git commit changes.bat
@@ -1,9 +1,39 @@
 @echo ON
 
+setlocal enabledelayedexpansion
 set gitPath="C:\Program Files (x86)\Git\bin\sh.exe"
 
-set /p _commitMessage=Enter the commit message :
+echo all: %*
 
-%gitPath% --login -i -c "git add -A" "%~1" && %gitPath% --login -i -c "git commit -am'%_commitMessage%' " "%~1"
+for %%a in ( %* ) do (        
+  set /p "commitMessage=1 commit message change in %%a : "
+  call :MAP %%a "!commitMessage!"
+  REM : need to add this line otherwise will run twice
+  echo %%a processed
+)
 
+:MAP
+setlocal
+echo argument 1: %1
+echo argument 2 : %2
+
+set "commitMessage=%2"
+REM replace double quotes with single quotes
+set commitMessage=!commitMessage:"='!
+
+
+IF [%~x1]==[] (
+
+  %~d1
+  cd "%~1"
+  REM for folder        
+  call %gitPath% --login -i -c "git add -A" "%1"
+  call %gitPath% --login -i -c "git commit -am%commitMessage%" %1
+  exit /b 0
+   
+) 
+endlocal
+exit /b 0
+  
+endlocal
 pause
