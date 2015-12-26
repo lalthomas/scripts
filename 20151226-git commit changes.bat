4b9f7b90d2c318e95bef9d9bb985f99f3c094aa2
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-26 23:08:44 +0530

    optimized and silent echo, pause

--- 20151226-git commit changes.bat
+++ 20151226-git commit changes.bat
@@ -2,46 +2,36 @@
 
 setlocal enabledelayedexpansion
 set gitPath="C:\Program Files (x86)\Git\bin\sh.exe"
-
-echo all: %*
+REM echo all: %*
 
 for %%a in ( %* ) do (        
   set /p "commitMessage=commit message %%a : "
-  call :MAP %%a "!commitMessage!"
-  REM : need to add this line otherwise will run twice
-  echo %%a processed
+  call :MAP %%a "!commitMessage!"  
 )
+
+exit /b 0
 
 :MAP
 setlocal
-echo argument 1: %1
-echo argument 2 : %2
+REM echo argument 1: %1
+REM echo argument 2 : %2
 
 set "commitMessage=%2"
 REM replace double quotes with single quotes
 set commitMessage=!commitMessage:"='!
-
 
 IF [%~x1]==[] (
   REM for folder 
   %~d1
   cd "%~1"      
   call %gitPath% --login -i -c "git add -A" "%1"
-  call %gitPath% --login -i -c "git commit -am%commitMessage%" %1
-  exit /b 0
-   
+  call %gitPath% --login -i -c "git commit -am%commitMessage%" %1     
 ) ELSE ( 
-
   REM for file
   %~d1
   cd "%~p1"
   call %gitPath% --login -i -c "git add '%~dpnx1'" "%~dp1"
-  call %gitPath% --login -i -c "git commit -am%commitMessage%" "%~dp1"
-  exit /b 0
-  
+  call %gitPath% --login -i -c "git commit -am%commitMessage%" "%~dp1"  
  )
 endlocal
-exit /b 0
-  
-endlocal
-pause
+exit /b 0  
