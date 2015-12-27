fe8c5493068473cacceced4befb8e744d2342391
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-27 12:34:23 +0530

    update the remote name and folder name of merging

--- 20151227-combine git repositories from a filelist.sh
+++ 20151227-combine git repositories from a filelist.sh
@@ -15,12 +15,15 @@ S=0
 while IFS='' read -r line
 do 
     S=$((S +1))
+	folderName=${line##*\\}
+	folderNameDashed=${folderName// /-}
+	echo "$folderName"
 	# echo "text read from file: $line"		
 	# folderPath=$( printf '%q' "$line" )			
-	git remote add folderName$S "$line"
-	git fetch folderName$S
-	git merge -s ours --no-commit folderName$S/master
-	git read-tree --prefix=folderName$S/ -u folderName$S/master
-	git commit -m "merging folderName$S into subdirectory"
+	git remote add "$folderNameDashed" "$line"
+	git fetch "$folderNameDashed"
+	git merge -s ours --no-commit "$folderNameDashed"/master
+	git read-tree --prefix="$folderName"/ -u "$folderNameDashed"/master
+	git commit -m "merging ""$folderName"" into subdirectory"
 done < "$1"
 
