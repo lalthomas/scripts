06b48c7882abdbe3fe2853f3e77030a933067d45
Lal Thomas <lal.thomas.mail@gmail.com>
2015-12-27 13:10:17 +0530

    beautify code and add support for parent folder with spaces

--- 20151227-combine git repositories from a filelist.sh
+++ 20151227-combine git repositories from a filelist.sh
@@ -9,29 +9,33 @@
 # d:\repo 2\
 #
 #
-read -p "enter a name for combined repo : " path
-cd $path
-mkdir parent
-cd parent
-git init
-touch .gitignore
-git add .
-git commit -m "initial commit"
 
-S=0
-# thanks: http://www.stackoverflow/a/10929511
-while IFS='' read -r line
-do 
-    S=$((S +1))
-	folderName=${line##*\\}
-	folderNameDashed=${folderName// /-}
-	# echo "$folderName"
-	# echo "text read from file: $line"		
-	# folderPath=$( printf '%q' "$line" )			
-	git remote add "$folderNameDashed" "$line"
-	git fetch "$folderNameDashed"
-	git merge -s ours --no-commit "$folderNameDashed"/master
-	git read-tree --prefix="$folderName"/ -u "$folderNameDashed"/master
-	git commit -m "merging ""$folderName"" into subdirectory"
-done < "$1"
+# input for output repo path is unix style i.e. /d/folder1/folder2
+read -p "enter unix path for combined repo : " path
 
+if [ -d "$path" ]; then
+ cd "$path"
+ mkdir parent
+ cd parent
+ git init
+ touch .gitignore
+ git add .
+ git commit -m "initial commit"
+
+ # thanks: http://www.stackoverflow/a/10929511
+ while IFS='' read -r line
+ do 
+     S=$((S +1))
+	 folderName=${line##*\\}
+	 folderNameDashed=${folderName// /-}
+	 # echo "$folderName"
+	 # echo "text read from file: $line"		
+	 # folderPath=$( printf '%q' "$line" )			
+	 git remote add "$folderNameDashed" "$line"
+	 git fetch "$folderNameDashed"
+	 git merge -s ours --no-commit "$folderNameDashed"/master
+	 git read-tree --prefix="$folderName"/ -u "$folderNameDashed"/master
+	 git commit -m "merging ""$folderName"" into subdirectory"
+ done < "$1"
+
+fi
