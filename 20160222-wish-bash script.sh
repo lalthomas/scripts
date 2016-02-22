49d11d6afc8bdb0ecd2a4f0b5d02b1ee9998e6dd
Lal Thomas <lal.thomas.mail@gmail.com>
2016-02-22 20:15:03 +0530

    add wish bash script

---
+++ 20160222-wish-bash script.sh
@@ -0,0 +1,13 @@
+# Filename : 20160222-wish-bash script.sh
+# Author : Lal Thomas 
+# Date : 2016-02-22
+# © Lal Thomas (lal.thomas.mail@gmail.com)
+
+alias wish=main
+
+main(){
+
+arguments="$@"
+echo $arguments
+
+}
