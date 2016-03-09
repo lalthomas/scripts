7101bf3f747926446e734ba417b8831ae591e330
Lal Thomas <lal.thomas.mail@gmail.com>
2016-03-09 10:43:22 +0530

    moved the script content to do folder bash

--- 20150917-do script get all projects and context.sh
+++
@@ -1,7 +0,0 @@
-
-for file in *.txt *.md
-do
- # do something on "$file"
- grep -o '[^  ]*+[^  ]\+'  "$file" | grep '^+' | sort -u >>inbox.txt
- grep -o '[^  ]*@[^  ]\+' "$file" | grep '^@' | sort -u >>inbox.txt
-done
