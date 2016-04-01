48fea42cf24cebf5de0c9853dc6e6c43d740188f
Lal Thomas <lal.thomas.mail@gmail.com>
2016-04-01 11:32:51 +0530

    update the script with exit value

--- 20160401-add todo to inbox-dos batch script.bat
+++ 20160401-add todo to inbox-dos batch script.bat
@@ -1,2 +1,3 @@
- REM thanks http://www.simplehelp.net/2008/10/20/launchy-basic-weby-tricks-and-appending-to-a-text-file/
- echo %*>>"D:\Dropbox\action\20140310-do\inbox.md"
+REM thanks http://www.simplehelp.net/2008/10/20/launchy-basic-weby-tricks-and-appending-to-a-text-file/
+echo %*>>"D:\Dropbox\action\20140310-do\inbox.md"
+exit
