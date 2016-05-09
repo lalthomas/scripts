ad234c9ce273e780cf519b792a10f2906d43e912
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-09 13:18:53 +0530

    update the script with clean docs procedure

--- 20160509-inbox folder-bash script.sh
+++ 20160509-inbox folder-bash script.sh
@@ -11,21 +11,38 @@ currentScriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 alias inbox=_inbox_
 
 _inbox_(){	
+
+	convert2lowercase(){
+	
+		echo "Converting filenames to lowercase"
+		return name
+	}
+
+	
+	clean_docs_names(){
+	
+		for f in *; do mv "$f" "`echo $f | tr "[:upper:]" "[:lower:]"`"; done
+		
+	}
+	
 	
 	usage(){
 
         echo 
         echo "Inbox OPTIONS"      
         echo " helper script to managing inbox folder"   
-        echo 
+        echo "clean_docs_names"
 		
 	}
+	
+	
 	
 	ACTION=$1
 	shift
 	
 	case "$ACTION" in		
-		help|usage)	usage ;;		
+		help|usage)	usage ;;
+		clean_docs_names) clean_docs_names;;
 	esac
 
 }
