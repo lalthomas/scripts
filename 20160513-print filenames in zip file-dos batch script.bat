33cc1b2052dc0a5e52e58d307f486e8e1b132388
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-13 19:26:45 +0530

    update the script for sendto use

--- 20160513-print filenames in zip file-dos batch script.bat
+++ 20160513-print filenames in zip file-dos batch script.bat
@@ -1,8 +1,11 @@
-@echo off
+@echo OFF
+setlocal
 REM Lal Thomas
 REM 2016-05-13
 REM Thank you http://stackoverflow.com/a/21559391/2182047
 REM Thank you http://stackoverflow.com/a/15568164/2182047
+%~d1
+cd %~p1
 set path=%path%;C:\Program Files\7-Zip
 set file=%1
 for /f "tokens=1,3 delims= " %%g in ('7z.exe l -slt %file%') do (  
