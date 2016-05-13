c625d9df1c95becd03887c1fabc6ed4b1c18319f
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-13 18:13:53 +0530

    add sample bash send to script

---
+++ 20160513-sendto menu sample-bash script.sh
@@ -0,0 +1,8 @@
+# a simple bash script to parse argument parsing from windows explorer send to menu
+# Lal Thomas
+# thanks : http://superuser.com/a/611470/318277
+
+# creating shortcut with target as 
+# C:\cygwin64\bin\bash.exe -c "'/cygdrive/d/Dropbox/action/20131027-scripts project/20160513-sendto menu sample-bash script.sh' $0; read"
+
+echo files $1 # should display file path
