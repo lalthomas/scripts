80db1970ba34bc0d3a552ebcd29b453c7bb1f5c7
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-13 19:33:48 +0530

    update for batch operation

--- 20160513-print filenames in zip file-dos batch script.bat
+++ 20160513-print filenames in zip file-dos batch script.bat
@@ -4,17 +4,32 @@ REM Lal Thomas
 REM 2016-05-13
 REM Thank you http://stackoverflow.com/a/21559391/2182047
 REM Thank you http://stackoverflow.com/a/15568164/2182047
-%~d1
-cd %~p1
-set path=%path%;C:\Program Files\7-Zip
-set file=%1
-for /f "tokens=1,3 delims= " %%g in ('7z.exe l -slt %file%') do (  
-  echo.%%g | findstr /b /c:"Path" 1>nul
-  if NOT errorlevel 1 (
-      echo.%%h >> %file%.txt
-	  REM to extract infromation from filename
-	  REM FOR %%i IN (%%h) DO (		
-	  REM ECHO %%~xi >> list.txt	  
-	  REM )	  	  
-  )  
+
+for %%f in (%*) do ( 
+ REM Thanks you http://stackoverflow.com/a/2541820
+ CALL :PROCESS %%f ) 
 )
+
+endlocal
+REM pause
+EXIT /b 0
+
+
+:PROCESS
+	setlocal	
+	%~d1
+	cd %~p1
+	set path=%path%;C:\Program Files\7-Zip
+	set file=%1
+	for /f "tokens=1,3 delims= " %%g in ('7z.exe l -slt %file%') do (  
+	  echo.%%g | findstr /b /c:"Path" 1>nul
+	  if NOT errorlevel 1 (
+		  echo processing %%h ...
+		  echo %%h >> %file%.txt
+		  REM to extract infromation from filename
+		  REM FOR %%i IN (%%h) DO (		
+		  REM ECHO %%~xi >> list.txt	  
+		  REM )	  	  
+	  )  
+	)
+EXIT /b 0
