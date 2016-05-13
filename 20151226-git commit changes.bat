31379ac2cd6776378554556cf476d298507d0a9d
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-13 21:03:30 +0530

    add diff display

--- 20151226-git commit changes.bat
+++ 20151226-git commit changes.bat
@@ -5,13 +5,32 @@ set gitPath="C:\Program Files (x86)\Git\bin\sh.exe"
 REM echo all: %*
 
 for %%a in ( %* ) do (  
-  echo.
+  echo.  
   echo change in : %%a
+  call :SHOWCHANGE %%a
   set /p "commitMessage=enter commit message : "
   call :MAP %%a "!commitMessage!"  
 )
+endlocal
 
 exit /b 0
+
+:SHOWCHANGE
+setlocal
+IF [%~x1]==[] (
+  REM for folder 
+  %~d1
+  cd "%~1"      
+  call %gitPath% --login -i -c "git diff" "%1"
+) ELSE ( 
+  REM for file
+  %~d1
+  cd "%~p1"        
+  call %gitPath% --login -i -c "git diff '%~nx1'" "%~dp1"  
+ )
+endlocal
+exit /b 0
+
 
 :MAP
 setlocal
