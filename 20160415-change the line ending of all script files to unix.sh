ff5258059ac0dbe250aced2cd12327eaea860a6b
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-24 18:56:33 +0530

    add credits and change to one line

--- 20160415-change the line ending of all script files to unix.sh
+++ 20160415-change the line ending of all script files to unix.sh
@@ -1,5 +1,5 @@
-for file in *.sh
-do
-# do something on "$file"
-dos2unix --force --m2d "$file"
-done
+# change the line ending of sh files to unix
+# Lal Thomas
+# 2016-05-24
+
+for file in *.sh ; do dos2unix --force --m2d "$file"; done
