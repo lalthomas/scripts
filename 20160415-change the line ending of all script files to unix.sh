ddbb21f7b7030e3676f52365683e7bd4135d818f
Lal Thomas <lal.thomas.mail@gmail.com>
2016-05-26 22:16:13 +0530

    update the script for unix to windows

--- 20160415-change the line ending of all script files to unix.sh
+++ 20160415-change the line ending of all script files to unix.sh
@@ -2,4 +2,8 @@
 # Lal Thomas
 # 2016-05-24
 
-for file in *.sh ; do dos2unix --force --m2d "$file"; done
+# replace ending of all sh files to unix
+for file in *.sh ; do dos2unix --force --d2u "$file"; done
+
+# replace ending of all md files to windows
+for file in *.md ; do dos2unix --force --u2d "$file"; done
