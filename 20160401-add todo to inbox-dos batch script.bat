6cc28b9fc5d6830ab6601f0f9e76b6d3976f246d
Lal Thomas <lal.thomas.mail@gmail.com>
2016-06-12 05:00:02 +0530

    add date support

--- 20160401-add todo to inbox-dos batch script.bat
+++ 20160401-add todo to inbox-dos batch script.bat
@@ -1,3 +1,17 @@
+@echo OFF
+REM Batch file to add to inbox.md file on do folder
+REM Lal Thomas
+REM 2016-04-01
+
 REM thanks http://www.simplehelp.net/2008/10/20/launchy-basic-weby-tricks-and-appending-to-a-text-file/
-echo %*>>"D:\Dropbox\action\20140310-do\inbox.md"
+REM Thanks http://stackoverflow.com/a/19706067/2182047
+REM Original modified for need
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "longdatestamp=%YYYY%-%MM%-%DD%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+echo %datestamp% %*>>"D:\Dropbox\action\20140310-do\inbox.md"
 exit
