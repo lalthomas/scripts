5c02bf46df5d61a4c98bd05668aa1bc7dfdfe248
Lal Thomas <lal.thomas.mail@gmail.com>
2016-08-09 11:01:03 +0530

    update the code create the readme if not present

--- 20160806-open readme file-dos batch script.bat
+++ 20160806-open readme file-dos batch script.bat
@@ -3,6 +3,19 @@ setlocal
 REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd %~p1
-start explorer "%~n1 readme.md"
+if exist "%~n1 readme.md" (	start explorer "%~n1 readme.md" ) else ( call :CREATEOPEN %1 )
+exit /b 0
 endlocal
-exit
+
+:CREATEOPEN
+Setlocal EnableDelayedExpansion
+set "SUBJECT=%~n1 Readme"
+set /p _Opt="redme file doesn't exist do you want tocreate (y/n) :"	
+IF /I "%_Opt%" == "y" ( 	
+	echo %% %SUBJECT% >>"%~n1 readme.md" ^
+	&& echo %% %DATE% >>"%~n1 readme.md" ^
+	&& echo %% Lal Thomas >>"%~n1 readme.md" ^
+	&& echo.>>"%~n1 readme.md" ^
+	&& start explorer "%~n1 readme.md"
+)
+exit /b 0
