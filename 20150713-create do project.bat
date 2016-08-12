efcc4b65eb17c2074d6f3e632953a5432a84d45f
Lal Thomas <lal.thomas.mail@gmail.com>
2016-08-12 12:11:05 +0530

    update the script with date, projectname subsitution on template

--- 20150713-create do project.bat
+++ 20150713-create do project.bat
@@ -1,4 +1,8 @@
-@echo off
+@echo OFF
+REM Author Lal Thomas (lal.thomas.mail@gmail.com)
+REM get the script folder path
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 
 REM Author Lal Thomas <lal.thomas.mail@gmail.com>
 REM Date LONGDATE
@@ -17,6 +21,18 @@ set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 set copyfilename="D:\Dropbox\support\20150619-home support template-todo project doc.md"
 set /p projectname="enter project name:"
 
-set filename="%datestamp%-%projectname%.md"
+set filename=%projectname:+=%
+set filename=%filename:\= %
+set filename=%filename:/= %
+set filename="%datestamp%-%filename% readme.md"
+
+REM generate file from the template 
 type %copyfilename% >> %filename%
-echo %projectname% >> %filename%
+
+set path="%scriptFolderPath%\tools\fart"
+fart -qC %filename% "PROJECTNAME" %projectname%
+fart -qC %filename% "DATE" %longdatestamp%
+start explorer %filename%
+
+REM pause
+endlocal
