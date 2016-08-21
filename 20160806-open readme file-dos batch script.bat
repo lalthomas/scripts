1702f23fade135aee5524a40196040ffb3a7a0f0
Lal Thomas <lal.thomas.mail@gmail.com>
2016-08-21 20:38:48 +0530

    add date support for the readme file script

--- 20160806-open readme file-dos batch script.bat
+++ 20160806-open readme file-dos batch script.bat
@@ -3,6 +3,16 @@ setlocal
 REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd %~p1
+
+REM Thanks http://stackoverflow.com/a/19706067/2182047
+REM Original modified for need
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "longdatestamp=%YYYY%-%MM%-%DD%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
 if exist "%~n1 readme.md" (	start explorer "%~n1 readme.md" ) else ( call :CREATEOPEN %1 )
 exit /b 0
 endlocal
@@ -13,7 +23,7 @@ set "SUBJECT=%~n1 Readme"
 set /p _Opt="redme file doesn't exist do you want tocreate (y/n) :"	
 IF /I "%_Opt%" == "y" ( 	
 	echo %% %SUBJECT% >>"%~n1 readme.md" ^
-	&& echo %% %DATE% >>"%~n1 readme.md" ^
+	&& echo %% %longdatestamp% >>"%~n1 readme.md" ^
 	&& echo %% Lal Thomas >>"%~n1 readme.md" ^
 	&& echo.>>"%~n1 readme.md" ^
 	&& start explorer "%~n1 readme.md"
