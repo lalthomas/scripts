a44b9abce2829beacc9352d941502a0ec8d3331a
Lal Thomas <lal.thomas.mail@gmail.com>
2016-10-19 20:51:58 +0530

    update the file have a generic way of making new files

--- 20160806-open supporting file-dos batch script.bat
+++ 20160806-open supporting file-dos batch script.bat
@@ -4,6 +4,14 @@ REM The following two line are Npp Hack for not changing the current path
 %~d1
 cd %~p1
 
+Setlocal EnableDelayedExpansion
+set "supportfiletype=%~2"
+set filename=%~n1 %supportfiletype%
+set fullpath=%~dp1%filename%
+REM add quotes
+set filename="%filename%"
+set fullpath="%fullpath%"
+
 REM Thanks http://stackoverflow.com/a/19706067/2182047
 REM Original modified for need
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
@@ -13,19 +21,26 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "longdatestamp=%YYYY%-%MM%-%DD%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-if exist "%~n1 readme.md" (	start explorer "%~n1 readme.md" ) else ( call :CREATEOPEN %1 )
+if exist %filename% ( "C:\Program Files (x86)\Notepad++\notepad++.exe" %fullpath% ) else ( CALL :CONFIRM )
 exit /b 0
 endlocal
 
-:CREATEOPEN
-Setlocal EnableDelayedExpansion
-set "SUBJECT=%~n1 readme"
-set /p _Opt="redme file doesn't exist do you want to create (y/n) :"	
-IF /I "%_Opt%" == "y" ( 	
-	echo %% %SUBJECT% >>"%~n1 readme.md" ^
-	&& echo %% %longdatestamp% >>"%~n1 readme.md" ^
-	&& echo %% Lal Thomas >>"%~n1 readme.md" ^
-	&& echo.>>"%~n1 readme.md" ^
-	&& start explorer "%~n1 readme.md"
+:CONFIRM
+setlocal
+set /p _Opt="%filename% doesn't exist do you want to create (y/n) :"	
+IF /I "%_Opt%" == "y" ( CALL :NEWFILE %filename% )
+endlocal
+exit /b 0
+
+:NEWFILE
+setlocal
+set "SUBJECT=%~1"
+if /I "%supportfiletype%" == "readme.md" (				
+	echo %% %SUBJECT% >>%filename% ^
+	&& echo %% %longdatestamp% >>%filename% ^
+	&& echo %% Lal Thomas >>%filename% ^
+	&& echo.>>%filename% ^
+	&& "C:\Program Files (x86)\Notepad++\notepad++.exe" %fullpath%
 )
+endlocal
 exit /b 0
