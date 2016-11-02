a88a8d828477a060ec031d99d7f7fe534751497d
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-02 22:54:58 +0530

    update the build prior knowledge script

--- 20161001-build prior knowledge-dos batch script.bat
+++ 20161001-build prior knowledge-dos batch script.bat
@@ -2,7 +2,6 @@
 echo Welcome to buid prior knowledge program
 pause
 
-
 cls
 echo starting inbox folder list
 echo.
@@ -13,4 +12,62 @@ echo  []
 start explorer "D:\Dropbox\reference\20150906-inbox folder list-dev gtd.txt"
 call "D:\Dropbox\action\20131027-scripts project\20150823-open folders from file list-dos script batch script.bat" "D:\Dropbox\reference\20150906-inbox folder list-dev gtd.txt"
 echo.
+pause
+
+echo starting google mail...
+echo.
+echo check for 
+echo.
+echo  [] inbox
+echo  [] spam
+echo  [] @action
+echo  [] @reference
+start explorer "https://mail.google.com/mail/u/0/?shva=1#label/%40action"
+
+pause
+start explorer "https://outlook.live.com/"
+pause
+start explorer "https://www.calendar.google.com/calendar/render?tab=mc&pli=1"
+pause
+start explorer "D:\Dropbox\action\20140310-do\contexts.md"
+pause
+start explorer "D:\Dropbox\action\20140310-do\done.txt"
+pause
+start explorer "D:\Dropbox\action\20140310-do\dont.md"
+pause
+start explorer "D:\Dropbox\action\20140310-do\dreams.md"
+pause
+start explorer "D:\Dropbox\action\20140310-do\projects.md"
+pause
+start explorer "D:\Dropbox\action\20140310-do\wishlist.md"
+pause
+start explorer "D:\Dropbox\reference\20150906-inbox folder list-dev gtd.txt"
+pause
+start explorer "D:\Dropbox\reference\20160115-horizon doc-life gtd.txt"
+pause
+start explorer "D:\Dropbox\reference\20160312-life lessons doc-life gtd.md"
+pause
+start explorer "D:\Dropbox\reference\20161001-active projects-dev gtd.txt"
+pause
+start explorer "D:\Dropbox\reference\20161006-trigger list-life gtd doc.md"
+pause
+start explorer "D:\Dropbox\reference\20140306-bookmarks doc-dev computer.txt"
+pause
+start explorer "D:\Dropbox\action\20140310-do\inbox.md"
+pause
+start explorer "D:\Inbox\doc\cleanup thunderbird\newstoday"
+pause
+start explorer "D:\Searches\life gtd checklist.search-ms"
+pause
+start explorer "D:\Searches\life gtd procedure.search-ms"
+pause
+start explorer "D:\Searches\life gtd projects.search-ms"
+pause
+start "" "C:\Program Files (x86)\Evernote\Evernote\ENScript.exe" showNotes /q "tag:""gtd""
+pause
+start "" "C:\Program Files (x86)\Evernote\Evernote\ENScript.exe" showNotes /q "tag:""lifehack""
+pause
+start "" "C:\Program Files (x86)\Evernote\Evernote\ENScript.exe" showNotes /q "tag:""workhack""
+pause
+start "" "C:\Program Files (x86)\Evernote\Evernote\ENScript.exe" showNotes /q "tag:""studyhack""
 pause
