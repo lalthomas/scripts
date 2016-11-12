5b210b1587b46df8821bdab6cf2652b9034c98bf
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-12 13:11:07 +0530

    add git support and fix the filename issue

--- 20160827-create file-dos batch script.bat
+++ 20160827-create file-dos batch script.bat
@@ -10,15 +10,23 @@ set scriptFolderPathFull=%~dp0%
 set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 %~d1
 cd %~dp1
-if exist %1 ( 
-	"C:\Program Files (x86)\Notepad++\notepad++.exe" %1 
+set filename=%1
+set commitmessage=%filename:"=\"%
+set commitmessage="add %commitmessage% file"
+
+if exist %filename% ( 
+	"C:\Program Files (x86)\Notepad++\notepad++.exe" %filename%
 	exit
 )
-set /p _Opt="do you want to create file %1 (y/n) :"	
+set /p _Opt="do you want to create file %filename% (y/n) :"	
 IF /I "%_Opt%" == "y" ( 	
 REM create file
-copy nul %1
-"C:\Program Files (x86)\Notepad++\notepad++.exe" %1
+copy nul %filename%
+"C:\Program Files (x86)\Notepad++\notepad++.exe" %filename%
+REM add to revision control
+git add %filename% "readme.md"
+git commit -m %commitmessage%
 )
+
 REM pause
 exit /b 0
