bf07336e5e60063b68040c92132aa6ad63ffc64e
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-12 13:08:53 +0530

    add support for command line arguments

--- 20160530-create reference file-dos batch script.bat
+++ 20160530-create reference file-dos batch script.bat
@@ -4,6 +4,7 @@ REM Date LONGDATE
 %~d1
 cd %1
 
+REM echo %*
 Setlocal EnableDelayedExpansion
 REM Thanks http://stackoverflow.com/a/19706067/2182047
 REM Original modified for need
@@ -14,27 +15,69 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "longdatestamp=%YYYY%-%MM%-%DD%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
+
+REM Get all parameters but not first
+REM Thanks http://stackoverflow.com/a/761658/2182047
+shift
+set params=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set params=%params% %1
+goto loop
+:afterloop
+
+
+REM set project name from second argument
+set projectname=%params%
+
 :NAME
-set /p projectname="enter reference file name:"
+call :VERIFY %projectname%
 set filename="%datestamp%-%projectname%.md"
 echo filename : %filename%
+set commitmessage=%filename:"=\"%
+set commitmessage="add %commitmessage% file"
 SET /p _Opt= Press Y for confirmation or N for retype : 
-IF /I "%_Opt%" == "N" ( goto :NAME )
 
-:CREATE
-echo %% %projectname% >> %filename%
-echo %% Lal Thomas >> %filename%
-echo %% %longdatestamp% >> %filename%
-echo.>> %filename%
-echo.>> %filename%
-echo Date		Notes >> %filename%
-echo ----------	------------------ >> %filename%
+IF /I "%_Opt%" == "N" ( 
+	call :VERIFY 
+	goto :NAME
+) ELSE (
+	goto :CONT 
+)
 
-REM add to revision control
-git add %filename%
-set message=%filename:"=\"%
-set message="add %message% file"
-git commit -m %message%
+:CONT
+REM reference file creation
 
+if not exist %filename% ( 
+	
+	echo %% %projectname% >> %filename%
+	echo %% Lal Thomas >> %filename%
+	echo %% %longdatestamp% >> %filename%
+	echo.>> %filename%
+	echo.>> %filename%
+	echo Date		Notes >> %filename%
+	echo ----------	------------------ >> %filename%
+	
+	REM lanuch
+	call :LAUNCH %filename%	
+	
+	REM add to revision control
+	git add %filename%
+	REM git commit -m %commitmessage%
+)
+
+REM launch exisitng file
+call :LAUNCH %filename%
+exit
+
+:VERIFY
+if [%1]==[] ( 
+set /p projectname="enter reference file name:" 
+)
+exit /b 0
+
+:LAUNCH
 REM start the program
-start "notepad-pp" %filename%
+start "notepad-pp" %1
+exit /b 0
