c9daa05ca12f56e01e17a542d6e7df90d548a72f
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-14 22:55:36 +0530

    add gtd review script

--- 20161114-gtd review-bash script.sh
+++ 20161114-gtd review-bash script.sh
@@ -6,6 +6,33 @@
 alias gsd='sh "$toolsRootPath/20161026-get-shit-done/get-shit-done.sh"'
 alias review=_review_main_
 
+build_prior_knoweledge(){
+	
+	# start google mail
+	cygstart 
+
+}
+
+reviewDay(){     
+    
+}
+
+reviewWeek(){
+
+    read -p "do you build prior knowledge : " opted
+	if [ $opted == "y" ]; then
+		build_prior_knoweledge
+	fi
+}
+
+reviewMonth(){
+    	
+}
+
+reviewYear(){
+
+}
+
 _review_main_(){
 
     # Get action
@@ -35,44 +62,23 @@ _review_main_(){
             else                
                case "$option" in
                     day)
-                        startDay
+                        reviewDay
                         ;;
                     week)
-                        startWeek
+                        reviewWeek
                         ;;                  
                     month)
-                        startMonth
+                        reviewMonth
                         ;;
                     year)
-                        startYear
+                        reviewYear
                         ;;
                     esac
             fi              
-            ;;
-        'end')
-            if [[ -z "$option" ]]; then
-               echo "workflow error : few arguments"
-               #adddonUsage
-            else                
-               case "$option" in
-                    day)
-                        endDay
-                        ;;
-                    week)
-                        endWeek
-                        ;;                  
-                    month)
-                        endMonth
-                        ;;
-                    year)
-                        endYear
-                        ;;
-                    esac
-            fi              
-            ;;
+            ;;        
         esac
     else
-        echo "workflow error: unrecognized option \"$option\"."
+        echo "review error: unrecognized option \"$option\"."
         echo "try \" view help\" to get more information."
     fi
 }
