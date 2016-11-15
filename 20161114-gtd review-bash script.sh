fb4428a328fb30942ed219401a567f91e6054006
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-15 13:24:56 +0530

    update the script to display todo items for each file

--- 20161114-gtd review-bash script.sh
+++ 20161114-gtd review-bash script.sh
@@ -3,82 +3,113 @@
 # Copyright (c) Lal Thomas, http://lalamboori.blogspot.in
 # License: GPL3, http://www.gnu.org/copyleft/gpl.html
 
-alias gsd='sh "$toolsRootPath/20161026-get-shit-done/get-shit-done.sh"'
 alias review=_review_main_
-
-build_prior_knoweledge(){
-	
-	# start google mail
-	cygstart 
-
-}
-
-reviewDay(){     
-    
-}
-
-reviewWeek(){
-
-    read -p "do you build prior knowledge : " opted
-	if [ $opted == "y" ]; then
-		build_prior_knoweledge
-	fi
-}
-
-reviewMonth(){
-    	
-}
-
-reviewYear(){
-
-}
+datafile=$1
 
 _review_main_(){
 
-    # Get action
-    action=$1
-    shift
+	itemsMatchPattern(){		 				
+		
+		pattern="$1"							
+		
+		counter=1		
+		while read -r  line;
+		do      							
+			FILENAMES[$counter]=$(echo $line | awk -F, '{print $1}'| tr -d '"')
+			counter=$((counter + 1))			
+		done < "$datafile" 
+		
+		# echo
+		# echo ${FILENAMES[@]}
+		# echo
+		
+		files_unique=($(printf "%s\n" "${FILENAMES[@]}" | sort -u))
+		
+		# for filename in "${files_unique[@]}"	
+		for ((i = 0; i < ${#FILENAMES[@]}; i++))	
+		do
+			counter=1
+			echo ${FILENAMES[i]}
+			safe_replacement=$(printf '%s\n' "${FILENAMES[i]}" | sed 's/[\&/]/\\&/g')        			
+			grep -e "$safe_replacement" "$datafile" | ( while read -r line;
+			do				
+				TODOS[$counter]=$(echo $line | awk -F, '{print $2}' | tr -d '"')										
+				counter=$((counter + 1))				
+			done && echo ${TODOS[@]})
+			
+		done	 
+										
+	}		
 
-    # Get option
-    option=$1;  
-    shift
+	build_prior_knoweledge(){
+		
+		pattern="."
+		# start google mail
+		itemsMatchPattern "$pattern"
+		
+	}
 
-    # Get rest of them
-    term="$@"
+	reviewDay(){     
+		
+		echo
+	}
 
-    # echo action: $action option: $option term: $term
+	reviewWeek(){
 
-    # Validate the input options
-    re="^(help|review)$"
-    if [[ "$action"=~$re ]]; then
-        case $action in
-        'help')
-            usage
-            ;;
-        'review')        
-            if [[ -z "$option" ]]; then
-               echo "review error : few arguments"
-               #adddonUsage
-            else                
-               case "$option" in
-                    day)
-                        reviewDay
-                        ;;
-                    week)
-                        reviewWeek
-                        ;;                  
-                    month)
-                        reviewMonth
-                        ;;
-                    year)
-                        reviewYear
-                        ;;
-                    esac
-            fi              
-            ;;        
-        esac
-    else
-        echo "review error: unrecognized option \"$option\"."
-        echo "try \" view help\" to get more information."
-    fi
+		build_prior_knoweledge
+		# read -p "do you build prior knowledge : " opted
+		# if [ $opted == "y" ]; then
+			
+		# fi
+	}
+
+	reviewMonth(){
+		echo
+	}
+
+	reviewYear(){
+		echo
+	}
+
+
+	# Get action
+	action=$1
+	shift
+
+	# Get option
+	option=$1;  
+	shift
+
+	# Get rest of them
+	term="$@"
+
+	# echo action: $action option: $option term: $term
+
+	# Validate the input options
+	re="^(help|review)$"
+	if [[ "$action"=~$re ]]; then
+		case $action in
+		'help')
+			usage
+			;;
+		'day')
+			reviewDay
+			;;
+		 'week')
+			reviewWeek
+			;;                  
+		 'month')
+			reviewMonth
+			;;
+		 'year')
+			reviewYear
+			;;
+			*)
+			echo "invalid option"
+			;;						    
+		esac
+	else
+		echo "review error: unrecognized option \"$option\"."
+		echo "try \" view help\" to get more information."
+	fi
 }
