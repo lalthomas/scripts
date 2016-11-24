edd8dcf61fe7106e7405a8f55b427f1f800c60cb
Lal Thomas <lal.thomas.mail@gmail.com>
2016-11-24 12:41:55 +0530

    add alias for move files from a list of file

--- 20161102-aliases for lalthomas pc-dos script support file.cmd
+++ 20161102-aliases for lalthomas pc-dos script support file.cmd
@@ -5,7 +5,7 @@ REM ‎2016-11-04 1252
 
 doskey +contact="D:\Dropbox\action\20131027-scripts project\20150710-create contact.bat" "D:\Dropbox\action\20150716-contacts"
 doskey +file="D:\Dropbox\action\20131027-scripts project\20160827-create file-dos batch script.bat" %CD%\$*
-doskey +inbox="D:\Dropbox\action\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\action\20140310-do\inbox.md" $*
+doskey +inbox="D:\Dropbox\action\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\action\20140310-do\inbox.txt" $*
 doskey +log="D:\Dropbox\action\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\action\20140310-do\log.txt" $*
 doskey +pomo="C:\Program Files (x86)\Hourglass\Hourglass.exe" $*
 doskey +reference="D:\Dropbox\action\20131027-scripts project\20160530-create reference file-dos batch script.bat" "D:\Dropbox\reference" $*
@@ -21,6 +21,7 @@ doskey make-email-summary="D:\Inbox\doc\cleanup thunderbird\temp\20161031-copy e
 doskey recyclebin-clean="D:\Dropbox\action\20131027-scripts project\20161020-empty recyclebin-dos batch script.bat"
 doskey tablet-copy="D:\Dropbox\action\20131027-scripts project\20140222-copy files from a list of files-dos batch script.bat" "D:\Dropbox\action\20140915-playlist\tablet-video-sync-list.m3u" "D:\temp"
 doskey wallpaper-default="D:/Dropbox/action/20131027-scripts project/20090411-set image as wallpaper-dos batch script.bat" "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+doskey copy-filelist="D:/Dropbox/action/20131027-scripts project/20161124-move files from a list of files-dos batch script.bat"
 
 set NLM=^
 
@@ -39,6 +40,7 @@ echo  +pomo
 echo  +reference
 echo  +timer
 echo  +todoproject
+echo  copy-filelist
 echo  gtd-action-files
 echo  gtd-inbox-files
 echo  gtd-review
