c58f3788ecc6d13bc36558eac5673cde3767fac1
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-04 14:47:05 +0530

    intergrated create contact module

--- 20150710-create contact.bat
+++ 20150710-create contact.bat
@@ -19,7 +19,7 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "longdatestamp=%YYYY%-%MM%-%DD%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-set copyfilename="D:\Dropbox\support\20140618-home support template-contact card.md"
+set copyfilename="D:\Dropbox\do\support\20140618-home support template-contact card.md"
 set /p name="enter contact name : "
 set /p namelower="enter contact name in lowercase : "
 set /p birthday="enter birthday (YYYY-MM-DD) : "
@@ -27,17 +27,50 @@ set /p homephonenumber="enter home phone number : "
 set /p email="enter email : "
 set /p facebookurl="enter facebook url : "
 
-set filename=%datestamp%-%namelower%.md
+set commitmessage="update the contact details of %namelower%"
+REM remove quotes
+set commitmessage="%commitmessage:"=\"%"
+
+set filename=%datestamp%-%namelower% contact file.md
 set fullfilepath="%CD%\%filename%"
-type %copyfilename% >> %fullfilepath%
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$LONGDATE$" "%longdatestamp%" >nul 2>nul
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$NAME$" "%name%" >nul 2>nul
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$BIRTHDAY$" "%birthday%" >nul 2>nul
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$NUMBER01$" "%homephonenumber%" >nul 2>nul
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$EMAIL$" "%email%" >nul 2>nul
-call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$URL01$" "%facebookurl%" >nul 2>nul
- 
-REM Open the file
-call explorer %fullfilepath%
+
+REM call create file routine
+call "%scriptFolderPath%\20160827-create file-dos batch script.bat" %fullfilepath%
+
+IF %ERRORLEVEL% EQU 0 ( 
+	
+	type %copyfilename% >> %fullfilepath%
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$LONGDATE$" "%longdatestamp%" >nul 2>nul
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$NAME$" "%name%" >nul 2>nul
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$BIRTHDAY$" "%birthday%" >nul 2>nul
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$NUMBER01$" "%homephonenumber%" >nul 2>nul
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$EMAIL$" "%email%" >nul 2>nul
+	call "%scriptFolderPath%\tools\fart\fart.exe" %fullfilepath% "$URL01$" "%facebookurl%" >nul 2>nul
+		
+	REM add to revision control	
+	git add %fullfilepath% >nul 2>nul
+	git commit -m %commitmessage% >nul 2>nul	
+	
+	IF %ERRORLEVEL% EQU 0 ( 
+		
+		echo SUCCESS : %filename% successfully updated and commited	
+		"C:\Program Files (x86)\Notepad++\notepad++.exe" %fullfilepath%
+		
+	) ELSE ( 
+		
+		echo ERROR : %filename%  is either not updated or commited 
+		
+	)	
+	
+	goto :end
+	
+) ELSE (
+	echo ERROR : %filename%  is either not created or commited	
+	exit /b 1
+)
+
+
+:end
+exit /b 0
 endlocal
-pause
+REM pause
