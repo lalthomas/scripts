7c4e510234455638eee4f6d938ab68c036767556
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-06 13:49:06 +0530

    add argument support

--- 20150713-create todo project.bat
+++ 20150713-create todo project.bat
@@ -19,9 +19,23 @@ set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "longdatestamp=%YYYY%-%MM%-%DD%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-set copyfilename="D:\Dropbox\support\20150619-home support template-todo project doc.md"
-set /p projectname="enter project name:"
-set /p upcoming="is this project upcoming (y/n)?"
+
+REM Get all parameters but not first
+REM Thanks http://stackoverflow.com/a/761658/2182047
+shift
+set params=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set params=%params% %1
+goto loop
+:afterloop
+
+REM set project name from second argument
+set projectname=%params%
+call :VERIFY %projectname%
+
+set copyfilename="D:\Dropbox\do\support\20150619-home support template-todo project doc.md"
 
 set filename=%projectname:+=%
 set filename=%filename:\= %
@@ -30,10 +44,7 @@ REM dont change the below two lines order
 set todofile="%datestamp%-%filename%.txt"
 set filename="%datestamp%-%filename% readme.md"
 
-IF /I "%upcoming%" == "y" ( 	
-	cd upcoming
-	echo %projectname%>%todofile%
-)
+call :PROJECTTYPE
 
 REM generate file from the template 
 type %copyfilename% >> %filename%
@@ -45,5 +56,41 @@ fart -qC %filename% "DATE" "%longdatestamp%" 2> nul
 start explorer %filename%
 REM pause
 endlocal
+exit
+
+:verify
+if [%1]==[] ( 
+set /p projectname="enter project name:"
+)
+exit /b 0
+
+
+:PROJECTTYPE
+
+REM upcoming type
+set /p upcoming="is this project upcoming (y/n)?"
+IF /I "%upcoming%" == "y" ( 				
+	cd upcoming
+	echo %projectname%>%todofile%	
+	exit /b 0
+) 
+
+REM archive type
+set /p archived="is this project archived (y/n)?"	
+IF /I "%archived%" == "y" ( 
+	cd archive	
+	echo %projectname%>%todofile%
+	exit /b 0
+) 	
+
+REM hold type
+set /p hold="is this project on hold (y/n)?"		
+IF /I "%hold%" == "y" ( 
+	cd hold
+	echo %projectname%>%todofile%
+	exit /b 0
+)	
+
+exit /b 0
 
 
