64745ff82aed9cf78c217ba327dbf66630f1dd86
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-17 09:50:38 +0530

    remove unused file

--- 20161107-create windows explorer shortcut-dos batch script.bat
+++
@@ -1,9 +0,0 @@
-@echo OFF
-REM Lal Thomas
-REM 2016-11-07
-
-
-REM Script (test.bat) to create a shortcut to itself 
-REM thanks http://stackoverflow.com/a/30029955/2182047
-
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%~n0.lnk');$s.TargetPath='%~f0';$s.Save()"
