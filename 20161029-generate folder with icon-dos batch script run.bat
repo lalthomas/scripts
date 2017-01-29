d0de1209e8d56c461c0bd538a4659c37c7a61b7c
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-29 14:37:14 +0530

    add fix the folder issue of readonly for portable thunderbird execution

--- 20161029-generate folder with icon-dos batch script run.bat
+++ 20161029-generate folder with icon-dos batch script run.bat
@@ -4,3 +4,4 @@ REM Creation Date : 2017-01-27
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20131027-scripts project\20161029-generate folder with icon-dos batch script.bat 	
 	
+"D:\Dropbox\project\20131027-scripts project\20161029-generate folder with icon-dos batch script.bat" . ""
---
+++ 20161125-remove readonly attriubute from thunderbird portable folder-dos batch script.bat
@@ -0,0 +1,4 @@
+@echo OFF
+d:
+cd "D:\PortableApps.com\PortableApps\ThunderbirdPortable"
+attrib -r -a -s -h /s /d
