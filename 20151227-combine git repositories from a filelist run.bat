f32280cdfc9e0d907eed5504ad9f016d3fb0e0de
Lal Thomas <lal.thomas.mail@gmail.com>
2017-02-06 10:08:38 +0530

    create "20151227-combine git repositories from a filelist run.bat" file and add it to 'readme.md' file listing section

---
+++ 20151227-combine git repositories from a filelist run.bat
@@ -0,0 +1,6 @@
+@echo OFF 	
+REM File : 20151227-combine git repositories from a filelist run.bat 	
+REM Creation Date : 2017-02-06 	
+REM Author : Lal Thomas 	
+REM Original File : D:\Dropbox\project\20131027-scripts project\20151227-combine git repositories from a filelist.sh 	
+	
