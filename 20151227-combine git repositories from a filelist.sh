117b8383be8dbb3903bce7ef17a998ad1c0d0299
Lal Thomas <lal.thomas.mail@gmail.com>
2017-02-06 11:52:56 +0530

    add support for commandline argument and parameter check

--- 20151227-combine git repositories from a filelist.sh
+++ 20151227-combine git repositories from a filelist.sh
@@ -15,25 +15,33 @@
 # - enter unix path for repository as . (dot), this will give the current directory path
 # - combined repo will be generated in folder called parent
 
-# Input to this script is file with list of git repositories"
+# filelist
+# Input to this script is file with list of git repositories
 #
 # file.txt
 # --------
 # d:\repo 1\
 # d:\repo 2\
-# 
 #
+filelist=$(cygpath -u "$1")
+echo $filelist
+FILE_LIST="$filelist"
+[ -r "$FILE_LIST" ] || read -p "enter filelist path for combined repo : " FILE_LIST 
+[ -r "$FILE_LIST" ] || echo "$1" "fatal error: cannot read filelist $FILE_LIST"
 
-
-
+# project path
 # input for output repo path is unix style i.e. /d/folder1/folder2
-read -p "enter unix path for combined repo : " path
+projectpath=$(cygpath -u "$2")
+echo $projectpath
+PROJECT_PATH="$projectpath"
+[ -r "$PROJECT_PATH" ] || read -p "enter unix path for combined repo : " PROJECT_PATH 
+[ -r "$PROJECT_PATH" ] || echo "$2" "fatal error: cannot read filelist $PROJECT_PATH"
 
-echo $path
+echo $PROJECT_PATH
 
-if [ -d "$path" ]; then
+if [ -d "$PROJECT_PATH" ]; then
  echo "building combined repo..."
- cd "$path"
+ cd "$PROJECT_PATH"
  mkdir parent
  cd parent
  git init
@@ -70,5 +78,5 @@ if [ -d "$path" ]; then
 	 git commit -m "merging ""$folderName"" into subdirectory"
 	 git remote remove "$folderNameDashed"
 	 
- done < "$1"
+ done < "$FILE_LIST"
 fi
