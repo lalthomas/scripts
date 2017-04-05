210f4dd7b2a8ae394bc0aa838c720c280d8a2294
Lal Thomas <lal.thomas.mail@gmail.com>
2017-04-05 17:08:51 +0530

    update the file

--- 20160225-reference folder-bash script.sh
+++ 20160225-reference folder-bash script.sh
@@ -1,5 +1,36 @@
-commitreference(){
+#!/bin/bash -x
 
-commitRepoChanges '$referenceRootPath'
+# Filename : 20160225-reference folder-bash script.sh
+# Author : Lal Thomas 
+# Date : 2017-04-05
+# © Lal Thomas (lal.thomas.mail@gmail.com)
 
+# initialize global variables 
+# do scripts variables
+
+currentScriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+alias referencefolder=_reference_main_
+
+_reference_main_(){
+	
+	commit(){
+
+		g repo commit '$referenceRootPath' "update files"
+	
+	}
+
+	contactCreate(){
+	
+		# thanks http://www.folkstalk.com/2012/07/bash-shell-script-to-read-parse-comma.html
+		INPUT_FILE='unix_file.csv'
+		IFS=','
+		while read OS HS
+		do
+		echo "Operating system - $OS"
+		echo "Hosting server type - $HS"
+		done < $INPUT_FILE
+	
+	}
+	
 }
+
