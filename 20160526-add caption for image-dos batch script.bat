f387b50b4a31f35c6c201b1a3972d70ac1ab1963
Lal Thomas <lal.thomas.mail@gmail.com>
2017-04-21 09:44:50 +0530

    update caption script

--- 20160526-add caption for image-dos batch script.bat
+++ 20160526-add caption for image-dos batch script.bat
@@ -10,11 +10,8 @@ set scriptFolderPathFull=%~dp0%
 set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 %~d1
 cd %~dp1
-echo Add Caption
-echo ===========
-echo.
-echo For filename as caption press enter
-SET /p _caption="Enter caption for the image : " 
+echo Add Caption:
+SET /p _caption="enter caption for the image ( enter for filename as caption ) : " 
 for %%f in (%*) do ( 
  REM Thanks you http://stackoverflow.com/a/2541820
  IF [%%~xf] == [] ( 
@@ -24,23 +21,23 @@ for %%f in (%*) do (
  )
 )
 endlocal
-pause
+REM pause
 EXIT /b 0
 
 :MAP
 setlocal
-REM if not /i %%~xf == .jpg ( goto :EOF )
 call :setImageMetaData %1
 endlocal
 EXIT /b 0
 
 :FOLDER
 setlocal
-REM SET /p _Opt="Are you sure to process all files on the folder(y/n)" 
-REM IF "%_Opt%" == "n" ( goto :EOF)
+REM SET /p _Opt="Are you sure to process all files on the folder \" %~n1 \" (y/n) : " 
+REM IF "%_Opt%" == "n" ( goto :EOR)
 echo batch processing folder : %1 
-REM for %%a in (%1\*.*) do ( CALL :MAP "%%a" )
-call :setImageMetaData %1
+for %%a in (%1\*.*) do ( CALL :MAP "%%a" )
+REM call :setImageMetaData %1
+:EOR
 endlocal
 EXIT /b 0
 
@@ -51,9 +48,8 @@ cd %~dp1
 set location=%1
 if [%_caption%]==[] set _caption=%~n1
 if /i %~x1 == .jpg (
-	call "%scriptFolderPath%\tools\exiftool\exiftool.exe" -overwrite_original_in_place -preserve -IPTC:Caption-Abstract="%_caption%" %location%
+	call "%scriptFolderPath%\tools\exiftool\exiftool.exe" -overwrite_original_in_place -preserve -IPTC:Caption-Abstract="%_caption%" %location%	
  )
-
 endlocal
 :END
 EXIT /b 0
