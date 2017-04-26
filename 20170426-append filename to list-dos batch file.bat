8750a17852b02ce24d1e93c0029c0f533c8e73e6
Lal Thomas <lal.thomas.mail@gmail.com>
2017-04-26 20:24:44 +0530

    fix the issue of script exiting when filename have special characters
    fixed by using !var! form

--- 20170426-append filename to list-dos batch file.bat
+++ 20170426-append filename to list-dos batch file.bat
@@ -4,14 +4,15 @@ REM Date : 2017-04-26
 setlocal
 
 REM filelist is passed as the first argument
-set filelist=%1
-REM echo %*
+set "filelist=%1"
+set "files=%*"
 
 REM select all parameters after first argument
 set /a count=1
 setlocal ENABLEDELAYEDEXPANSION
-for %%f in (%*) do (			
-	call :additem !count! %%f
+for %%f in (%files%) do (
+	REM echo %%f
+	call :additem !count! "%%~f"
 	set /a count=count+1
 )
 
@@ -21,10 +22,14 @@ goto :end
 
 
 :additem
+setlocal enableDelayedExpansion
 set /a count=%1
-set file=%~2
-if NOT %count% EQU 1 ( echo %file% >> %filelist% ) 
+set "file=%~2"
+
+REM !file! escapes the brackets in filename
+if NOT %count% EQU 1 ( echo !file!>> %filelist% ) 
 exit /b 0
+
 
 REM pause
 REM program end
