14a096e39c7bb6c4309ed69b47a9d36d1bd8ed12
Lal Thomas <lal.thomas.mail@gmail.com>
2017-05-14 13:26:51 +0530

    add commit feauture

--- 20160225-support folder-bash script.sh
+++ 20160225-support folder-bash script.sh
@@ -1,5 +1,20 @@
-commitsupport(){
+#!/bin/bash -x
+# Filename : 20160225-support folder-bash script.sh
+# Author : Lal Thomas 
+# Date : 2016-02-25
+# © Lal Thomas (lal.thomas.mail@gmail.com)
 
-	commitRepoChanges '$supportRootPath'
+# initialize global variables 
+currentScriptFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+alias sf=_support_main_
+
+_support_main_(){
+	
+	
+	commit(){
+
+		g repo commit '$supportRootPath' "update files"
+	
+	}
 	
 }
