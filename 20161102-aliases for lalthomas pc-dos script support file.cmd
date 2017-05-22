7443ddf9947645b025a6645de0fc458c853cb9d8
Lal Thomas <lal.thomas.mail@gmail.com>
2017-05-22 13:54:53 +0530

    update the paths

--- 20161102-aliases for lalthomas pc-dos script support file.cmd
+++ 20161102-aliases for lalthomas pc-dos script support file.cmd
@@ -6,7 +6,7 @@ REM ‎2016-11-04 1252
 doskey +contact="D:\Dropbox\project\20131027-scripts project\20150710-create contact.bat" "D:\Dropbox\do\reference" $*
 doskey +file="D:\Dropbox\project\20131027-scripts project\20160827-create file-dos batch script.bat" $*
 doskey +inbox="D:\Dropbox\project\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\do\inbox.txt" $*
-doskey +lesson="D:\Dropbox\project\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\do\reference\20160312-life lessons doc-life gtd.md" $*
+doskey +lesson="D:\Dropbox\project\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\do\lessons.txt" $*
 doskey +log="D:\Dropbox\project\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\do\log.txt" $*
 doskey +pomo="C:\Program Files (x86)\Hourglass\Hourglass.exe" $*
 doskey +reference="D:\Dropbox\project\20131027-scripts project\20160530-create reference file-dos batch script.bat" "D:\Dropbox\do\reference" $*
@@ -15,7 +15,7 @@ doskey +todoproject="D:\Dropbox\project\20131027-scripts project\20150713-create
 doskey copy-filelist="D:\Dropbox\project\20131027-scripts project\20161124-move files from a list of files-dos batch script.bat" $*
 doskey download-video="D:\Dropbox\project\20131027-scripts project\20170507-download video of url in clipboard.bat"
 doskey encode-image="D:\Dropbox\project\20131027-scripts project\20170514-copy base64 encoding of image file to clipboard.bat"
-doskey gtd-action-files="D:\Dropbox\project\20131027-scripts project\20150823-open folders from file list-dos script batch script.bat" "D:\Dropbox\do\reference\20161001-project list-dev gtd.txt"
+doskey gtd-action-files="D:\Dropbox\project\20131027-scripts project\20150823-open folders from file list-dos script batch script.bat" "D:\Dropbox\do\reference\20161001-gtd project list.txt"
 doskey hh=doskey /MACROS
 doskey jupyter-notebook=jupyter notebook --notebook-dir="%CD%"
 doskey log-meditate="D:\Dropbox\project\20131027-scripts project\20160401-add log entry to file-dos batch script.bat" "D:\Dropbox\do\log.txt" another great session of meditation with calm.com
