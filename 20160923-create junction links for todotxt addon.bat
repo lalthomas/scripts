357fc6ff8746fba42152d3bc0fda855b6651176a
Lal Thomas <lal.thomas.mail@gmail.com>
2017-05-22 13:53:13 +0530

    add script for creating junctions links for todotxt addon

---
+++ 20160923-create junction links for todotxt addon.bat
@@ -0,0 +1,12 @@
+@echo OFF
+REM Lal Thomas
+REM 2017-05-21
+
+MKLINK /J plan "D:\Dropbox\project\20150323-plan todotxt cli addon"
+MKLINK /J invalidate "D:\Dropbox\project\20151116-invalidate todotxt cli addon"
+MKLINK /J markdown "D:\Dropbox\project\20151117-markdown todotxt cli addon"
+MKLINK /J journal "D:\Dropbox\project\20160204-journal todotxt cli addon"
+MKLINK /J log "D:\Dropbox\project\20160204-log todotxt cli addon"
+MKLINK /J print "D:\Dropbox\project\20160204-print todotxt cli addon"
+MKLINK /J file "D:\Dropbox\project\20160222-file todotxt cli addon"
+MKLINK /J mdview "D:\Dropbox\project\20150211-mdview todotxt cli addon"
