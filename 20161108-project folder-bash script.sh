11b19a6d4a3120216a5c9f3000bc6aa592e7e3a7
Lal Thomas <lal.thomas.mail@gmail.com>
2017-06-09 13:13:26 +0530

    add support for cygwin and mingw
    - fix the issue of location
    - update the documentation

--- 20161108-project folder-bash script.sh
+++ 20161108-project folder-bash script.sh
@@ -10,17 +10,10 @@ alias project=_project_main_
 _project_main_(){
 
 	create(){		
-			
-		getProjectType(){
 		
-			echo
-			echo find the project repo type from opened file
-			start "" "d:\Dropbox\project\20131027-scripts project\20150715-git-bash script readme.md"
-			echo
-			read -p "enter project repo type and press [enter]: " projecttype
-			echo
-				
-		}
+		local projectname=""
+		local projecttype=""		
+		local projectfolder=""
 		
 		getProjectName(){
 			
@@ -28,58 +21,87 @@ _project_main_(){
 			
 		}
 		
-		argsno=$#		
-		local projecttype=$1
-		shift
-		local location=$1
-		shift
-		local projectname="$@"
-
+		getProjectType(){
+		
+			filetypereadme="d:\Dropbox\project\20131027-scripts project\20150715-git-bash script readme.md"			
+			echo find the project repo type from opened file
+			start "" "$filetypereadme" > /dev/null 2>&1 || cygstart "$filetypereadme" > /dev/null 2>&1			
+			read -p "enter project repo type and press [enter]: " projecttype			
+				
+		}
+		
+		makerepo(){
+		
+								
+			# get inside the repo
+			cd "$location"
+			mkdir -p "$projectfolder" && cd "$projectfolder"
+			
+			# add readme		
+			t markdown add H1 "ReadMe" "readme.md"
+		
+			# add gitignore
+			g repo add gitignore windows > /dev/null 2>&1
+			g repo add gitignore $projecttype > /dev/null 2>&1
+		
+			# intialize repo
+			g repo create "$PWD" > /dev/null 2>&1
+			echo 
+			echo "SUCCESS : Project repo created successfully"
+			
+			# add to inbox
+			addtoInbox
+			
+			# get out of repo
+			
+			# TODO: ask and open a new terminal on folder
+			# TODO: ask and open explorer on folder
+			
+			cd ..
+		
+		}
+		
+		addtoInbox(){
+	
+			# add to inbox list
+			local projectfolderpath=$(cygpath -d "$PWD")		
+			echo $projectfolderpath>>"$(cygpath -u "D:\Dropbox\do\inbox.txt")"
+			start "" "D:\Dropbox\do\inbox.txt" > /dev/null 2>&1 || cygstart "D:\Dropbox\do\inbox.txt"  > /dev/null 2>&1	
+		}
+	
+		argsno=$#				
+		# echo "#argument  : $argsno"
+		
 		if [ $argsno -eq 0 ];	
 		then	
-			getProjectType
-			location=$PWD
 			getProjectName
-		else   
-			if [ $argsno -eq 1 ];
-				then					
-					location=$PWD
-					getProjectName
-			else
-				if [ $argsno -eq 2 ]; 				
-				then
-					getProjectName
-				fi						
-			fi	   		   		
+			getProjectType						
+		elif [ $argsno -eq 1 ]; then		
+			location=$PWD
+			projectname="$@"			
+			getProjectType					
+		elif [ $argsno -eq 2 ]; then
+			projecttype=$1
+			shift 
+			projectname="$@"			
+		else 
+			echo "Invalid OPTIONS"
 		fi
 		
-		echo INFO: T:$projecttype L:$location N:$projectname
+		projectfolder="$today-$projectname"
 		
-		local projectpath="$location/$today-$projectname"
-		mkdir -p "$projectpath"		
+		# echo "INFO [ N: $projectname | T: $projecttype | L:$location ]"
 		
-		# get inside the repo
-		cd "$projectpath"
+						
+		# display info		
+		printf "\n%s" "PROJECT INFO"
+		printf "\n%s" "name     : $projectname"
+		printf "\n%s" "type     : $projecttype"		
+		printf "\n%s" "folder   : $projectfolder"
+		echo 
 		
-		# add readme		
-		t markdown add H1 "ReadMe" "readme.md"
+		makerepo
 		
-		# add gitignore
-		g repo add gitignore windows >/dev/null
-		g repo add gitignore $projecttype >/dev/null
-		
-		# intialize repo
-		g repo create "$projectpath" >/dev/null
-		echo "SUCCESS : Project repo created successfully"
-		
-		# add to inbox list
-		projectfolder=$(cygpath -d "$PWD")		
-		echo $projectfolder>>"$(cygpath -u "D:\Dropbox\do\inbox.txt")"
-		
-		start "" "D:\Dropbox\do\inbox.txt"
-		
-		# get out of repo		
-		cd ..
 	}
 
 	usage(){
@@ -89,28 +111,26 @@ _project_main_(){
         echo " helper script to managing do folder"   
         echo 
         echo "OPTIONS are..."
-        echo 		
-		echo "create [type] [path] [name]"		
-		echo "create [path] [name]"		
-		echo "create [name]"		
+        echo 				
+		echo "create [type] [name]"		
+		echo "create [name]"
+		echo "create"
 		echo 
-		echo "e.g. project create Python . python workouts"
-		echo
-		echo " Use only Migw Bash "
+		echo "e.g. project create \"Python\" \".\" \"python workouts\""
 		echo 
 		
 	}
 	
-	project=$1
+	option=$1
 	shift
 	
 	# test the script
-	# echo $filename $project
+	# echo $filename $option
 
 	pushd "D:\Dropbox\project" > /dev/null 2>&1
-	case "$project" in		
-		create) create $@;;
-		help|usage)	usage ;;		
+	case "$option" in		
+		create) create "$@";;
+		help|usage)	usage ;;
 	esac	
 	popd  > /dev/null 2>&1
 	
