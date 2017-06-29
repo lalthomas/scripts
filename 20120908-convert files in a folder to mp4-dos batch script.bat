a9b5a56a30b3ec7c1743e4885f10321b9bbafa57
Lal Thomas <lal.thomas.mail@gmail.com>
2017-06-29 15:41:08 +0530

    update the script for parameters

--- 20120908-convert files in a folder to mp4-dos batch script.bat
+++ 20120908-convert files in a folder to mp4-dos batch script.bat
@@ -1,3 +1,12 @@
+@echo OFF
+setlocal
+%~d1
+cd %~p1
+REM find script folder 
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
 set path="%PATH%;%CD%
-for %%a in ("%CD%"\*.*) do ( call E:\Devel\Mis\Video\file2mp4.bat "%%a" )
+for %%a in ("%CD%"\*.*) do ( call "%scriptFolderPath%20130317-convert file to mp4-dos batch script.bat" "%%a" )
+
 REM pause
+endlocal
