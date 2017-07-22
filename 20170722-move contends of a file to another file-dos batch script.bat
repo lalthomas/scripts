4a83999e456436a01165468a5b2bd3d07cd0d419
Lal Thomas <lal.thomas.mail@gmail.com>
2017-07-23 00:32:00 +0530

    update the file

--- 20170722-move contends of a file to another file-dos batch script.bat
+++ 20170722-move contends of a file to another file-dos batch script.bat
@@ -5,10 +5,15 @@ REM Author : Lal Thomas
 REM 2017-07-22	
 REM first argument is the source file
 REM second argument is the destination file
+
+REM add new line to destination file
 echo.>>%2
+
+REM add contends of source file to destination file
 type %1 >>%2
 
-REM create unicode file
-chcp 65001
+REM empty source file
 echo.>%1
+
+REM pause
 endlocal
