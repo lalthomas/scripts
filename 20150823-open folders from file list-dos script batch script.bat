5bdaead1d4cd3ba8876146d060cf30b6ce010d52
Lal Thomas <lal.thomas.mail@gmail.com>
2017-09-28 22:02:56 +0530

    add spacing

--- 20150823-open folders from file list-dos script batch script.bat
+++ 20150823-open folders from file list-dos script batch script.bat
@@ -2,7 +2,10 @@
 setlocal
 cd "%~dp1"
 set "filename=%~dpnx1"
+
 for /f "tokens=*" %%a in ( 'type "%filename%"' ) do (  
-  echo %%a | findstr /r "^\#" >nul && ( echo . ) || ( start explorer "%%a" )    
+  
+  echo %%a | findstr /r "^\#" >nul && ( echo . ) || ( start explorer "%%a" )
+  
 )
 endlocal
