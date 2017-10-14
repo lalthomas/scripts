3e964fa4663ae6d408d06b446a88143f12803957
Lal Thomas <lal.thomas.mail@gmail.com>
2017-10-14 06:31:58 +0530

    add credits

---
+++ 20171008-add string to file based on key -bash script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# Filename : 20171008-add string to file based on key -bash script.sh
+# Author : Lal Thomas 
+# Date : 2017-10-08
+# © Lal Thomas (lal.thomas.mail@gmail.com)
+
+args="$@"
+
