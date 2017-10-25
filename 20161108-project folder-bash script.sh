9e20ab79287d163b1371d2fdb4c8cc4abbfb3ab7
Lal Thomas <lal.thomas.mail@gmail.com>
2017-10-26 00:07:11 +0530

    update project folder helper script
    - user input of project type

--- 20161108-project folder-bash script.sh
+++ 20161108-project folder-bash script.sh
@@ -9,109 +9,59 @@ alias project=_project_main_
 
 _project_main_(){
 
-	create(){		
+	create(){
 		
 		local projectname=""
-		local projecttype=""		
-		local projectfolder=""
 		
-		getProjectName(){
-			
-			read -p "enter project name and press [enter]: " projectname
-			
-		}
-		
-		getProjectType(){
-		
-			filetypereadme="D:\project\20131027-scripts project\20150715-git-bash script readme.md"			
-			echo find the project repo type from opened file
-			start "" "$filetypereadme" > /dev/null 2>&1 || cygstart "$filetypereadme" > /dev/null 2>&1			
-			read -p "enter project repo type and press [enter]: " projecttype			
-				
-		}
-		
-		argsno=$#				
+		argsno=$#
 		# echo "#argument  : $argsno"
 		
-		if [ $argsno -eq 0 ];	
-		then	
-			getProjectName
-			getProjectType						
-		elif [ $argsno -eq 1 ]; then		
-			location=$PWD
-			projectname="$@"			
-			getProjectType					
-		elif [ $argsno -eq 2 ]; then
-			projecttype=$1
-			shift 
-			projectname="$@"			
-		else 
+		if [ $argsno -eq 0 ];
+		then
+			read -p "enter project name and press [enter]: " projectname
+		elif [ $argsno -gt 1 ]; then
+			projectname="$@"
+		else
 			echo "Invalid OPTIONS"
 		fi
 		
-		projectfolder="$today-$projectname"
-		
-		# echo "INFO [ N: $projectname | T: $projecttype | L:$location ]"
-		
-						
-		# display info		
-		printf "\n%s" "PROJECT INFO"
-		printf "\n%s" "name     : $projectname"
-		printf "\n%s" "type     : $projecttype"		
-		printf "\n%s" "folder   : $projectfolder"
-		echo 
-				
-		
+		local projectfolder="$today-$projectname"
+		# echo "INFO [ N: $projectname | L:$location ]"
+		printf "\n%s" "creating \"$projectfolder\" project repository "
 		# return
-			
-		##################
 		# repo creation
-		##################
 		
-		# get inside the repo		
+		# get inside the repo
 		mkdir -p "$projectfolder" && cd "$projectfolder"
-		
-		# add readme		
-		t markdown add H1 "ReadMe" "readme.md"
-	
-		# add gitignore
-		g repo add gitignore windows > /dev/null 2>&1
-		g repo add gitignore $projecttype > /dev/null 2>&1
-	
-		# intialize repo
-		g repo create "$PWD" > /dev/null 2>&1
-		echo 
+		# echo $PWD
+		gh repo create		
+		gh repo add gitignore		
+		t markdown add H1 "ReadMe" "readme.md" && gh repo stage file readme.md && gh repo commit "add readme.md"
 		echo "SUCCESS : Project repo created successfully"
 		
-		# add to inbox.txt
-		local projectfolderpath=$(cygpath -d "$PWD")		
-		echo "$longdate add \"$projectfolderpath\" to todo project file" >>$(cygpath -u "D:\do\inbox.txt")
-		start "" "D:\do\inbox.txt" > /dev/null 2>&1 || cygstart "D:\do\inbox.txt"  > /dev/null 2>&1	
-
+		# add gtd inbox
+		gtd inbox add filepath "$PWD"
 		
 		# get out of repo
-		
 		# TODO: ask and open a new terminal on folder
 		# TODO: ask and open explorer on folder
-		
 		cd ..
-				
+		
 	}
 
 	usage(){
 		
 		echo 
-        echo "project OPTIONS"      
-        echo " helper script to managing do folder"   
+        echo "project OPTIONS"
+        echo " helper script to managing do folder"
         echo 
         echo "OPTIONS are..."
-        echo 				
-		echo "create [type] [name]"		
-		echo "create [name]"
+        echo 	
 		echo "create"
+		echo "create [name]"
 		echo 
-		echo "e.g. project create \"Python\" \"python workouts\""
-		echo "e.g. project create \"c\" \"c programs\""
+		echo "e.g. project create \"python workouts\""
+		echo "e.g. project create \"c programs\""
 		echo 
 		
 	}
