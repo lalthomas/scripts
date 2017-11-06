2a179c6cd3ff6f85266bce36af7f7c5a28e6ca59
Lal Thomas <lal.thomas.mail@gmail.com>
2017-11-06 16:52:03 +0530

    fix variable name conflit

--- 20160222-wish-bash script.sh
+++ 20160222-wish-bash script.sh
@@ -3,16 +3,16 @@
 # � Lal Thomas (lal.thomas.mail@gmail.com)
 
 # get the file to process
-filename=$1
+wishfile=$1
 shift
 
 alias wish=_wish_main_
 
 _wish_main_(){  
 	
-	today_match_pattern="^\"[0-9]\{4\}-$monthCount-$dayCount\""
-	yesterday_match_pattern="^\"[0-9]\{4\}-$(date --date='yesterday' +'%m-%d')\""
-	tomorrow_match_pattern="^\"[0-9]\{4\}-$(date --date='tomorrow' +'%m-%d')\""
+	local today_match_pattern="^\"[0-9]\{4\}-$monthCount-$dayCount\""
+	local yesterday_match_pattern="^\"[0-9]\{4\}-$(date --date='yesterday' +'%m-%d')\""
+	local tomorrow_match_pattern="^\"[0-9]\{4\}-$(date --date='tomorrow' +'%m-%d')\""
 	
     dieWithHelp(){
         case "$1" in
@@ -39,7 +39,7 @@ _wish_main_(){
         read -p "enter comment : " comment
         read -p "enter date of occasion YYYY-MM-DD : " occasionDate 
         # echo the lowercase
-        echo "$occasionDate | $name <$address> | $occasion | $comment" | awk '{print tolower($0)}' >>"$filename"    
+        echo "$occasionDate | $name <$address> | $occasion | $comment" | awk '{print tolower($0)}' >>"$wishfile"    
         echo "entry : \"$occasionDate | $name <$address> | $occasion | $comment\" added successfully..."
     
     }
@@ -57,7 +57,7 @@ _wish_main_(){
 			fi
 			
 			counter=1			
-			grep -e $pattern "$filename" | while read -r  line ;
+			grep -e $pattern "$wishfile" | while read -r  line ;
 			do      				
 				name=$(echo $line | awk -F, '{print $2}'| tr -d '"')
 				occasion=$(echo $line | awk -F, '{print $3}' | tr -d '"')
@@ -72,7 +72,7 @@ _wish_main_(){
 			done				
 		}	
 			
-	# cat --number "$filename"			
+	# cat --number "$wishfile"			
 	
 	email(){		
 		OPTION=$1	
@@ -117,7 +117,7 @@ _wish_main_(){
 	}
 	   
     openfile(){      
-      cygstart "$filename"
+      cygstart "$wishfile"
     }
 
     usage(){
