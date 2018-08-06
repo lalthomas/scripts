1b02741943e2cf75133c9674dc48053f6bf9326e
lalthomas <lal.thomas.mail@gmail.com>
2018-08-06 19:05:05 +0530

    split the script to two files
    - one for favorite and other for any application

---
+++ 20180806-create shortcut to startup for any application-powershell script.ps1
@@ -0,0 +1,31 @@
+# thanks https://stackoverflow.com/a/31602095
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# thanks : https://code.adonline.id.au/folder-file-browser-dialogues-powershell/
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    Multiselect = $false # Multiple files can be chosen
+	Filter = 'Executables (*.exe, *.bat,*.sh)|*.exe;*.bat;*.sh' # Specified file types
+} 
+[void]$FileBrowser.ShowDialog()
+
+If($FileBrowser.FileNames -like "*\*") {
+
+	# Do something 
+	$FileBrowser.FileName #Lists selected files (optional)
+	$filepath = $FileBrowser.FileName;
+	# thanks https://stackoverflow.com/a/35813307
+	$filename = Split-Path $filepath -leaf
+		
+}
+else {
+    Write-Host "Cancelled by user"
+}
+
+# create link on start up folder
+# thanks https://www.anoopcnair.com/windows-10-add-applications-startup-msconfig/
+$StartUp="$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+# New-Item -ItemType SymbolicLink -Path "$StartUp" -Name "Calculator.lnk" -Value "c:\windows\system32\calc.exe"
+New-Item -ItemType SymbolicLink -Path "$StartUp" -Name $filename -Value $filepath
+# Pause
+# Read-Host -Prompt "Press Enter to continue"
--- 20180806-create shortcut to startup menu for favourite apps-powershell script.ps1
+++ 20180806-create shortcut to startup menu for favourite apps-powershell script.ps1
@@ -1,59 +1,12 @@
-
-Set-ExecutionPolicy Unrestricted
-
-# thanks : https://powershell.org/forums/topic/powershell-ruan-as-administrator/
-
-function IsAdministrator
-{
-    $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-    $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
-    $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
-}
-
-function IsUacEnabled
-{
-    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
-}
-
-if (!(IsAdministrator))
-{
-    if (IsUacEnabled)
-    {
-        [string[]]$argList = @('-NoProfile', '-NoExit', '-File', $MyInvocation.MyCommand.Path)
-        $argList += $MyInvocation.BoundParameters.GetEnumerator() | Foreach {"-$($_.Key)", "$($_.Value)"}
-        $argList += $MyInvocation.UnboundArguments
-        Start-Process PowerShell.exe -Verb Runas -WorkingDirectory $pwd -ArgumentList $argList 
-        return
-    }
-    else
-    {
-        throw "You must be administrator to run this script"
-    }
-}
-
-# thanks : https://code.adonline.id.au/folder-file-browser-dialogues-powershell/
-
-Add-Type -AssemblyName System.Windows.Forms
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    Multiselect = $false # Multiple files can be chosen
-	Filter = 'Executables (*.exe, *.bat,*.sh)|*.exe;*.bat;*.sh' # Specified file types
-} 
-[void]$FileBrowser.ShowDialog()
-
-If($FileBrowser.FileNames -like "*\*") {
-
-	# Do something 
-	$FileBrowser.FileName #Lists selected files (optional)
-	$filepath = $FileBrowser.FileName;
-	# thanks https://stackoverflow.com/a/35813307
-	$filename = Split-Path $filepath -leaf
-		
-}
-else {
-    Write-Host "Cancelled by user"
-}
+# thanks https://stackoverflow.com/a/31602095
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 # create link on start up folder
-
+# thanks https://www.anoopcnair.com/windows-10-add-applications-startup-msconfig/
 $StartUp="$Env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-New-Item -ItemType SymbolicLink -Path "$StartUp" -Name $filename -Value $filepath
+
+# lauchy
+New-Item -ItemType SymbolicLink -Path "$StartUp" -Name "launchy.lnk" -Value "D:\Portable\launchy\Launchy.exe"
+
+# Pause
+# Read-Host -Prompt "Press Enter to continue"
