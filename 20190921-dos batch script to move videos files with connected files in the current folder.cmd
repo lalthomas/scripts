27af47af76c87787d5063ca1476e6d75bfdabdb2
lalthomas <lal.thomas.mail@gmail.com>
2019-09-28 12:24:40 +0530

    add script file

---
+++ 20190921-dos batch script to move videos files with connected files in the current folder.cmd
@@ -0,0 +1,76 @@
+﻿
+REM move film files and associated files to OK folder
+
+
+for %%f in (*.flv)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+ )
+
+
+
+for %%f in (*.avi)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+ )
+
+
+for %%f in (*.divx) do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.m4v)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.mkv)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.mp4)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.mpg)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.vob)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
+
+for %%f in (*.rmvb)  do ( 
+
+	move "%%~dpnf.*" OK 
+	move "%%~dpnf-fanart.*" OK
+	move "%%~dpnf-poster.*" OK
+
+)
