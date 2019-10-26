9cf8fa381763de4fb0c65983c9ccbcbd6111a0e3
lalthomas <lal.thomas.mail@gmail.com>
2019-10-26 12:57:31 +0530

    update

--- 20191026-show slideshow of wallpapers dos batch script.bat
+++ 20191026-show slideshow of wallpapers dos batch script.bat
@@ -1,31 +1,39 @@
-@echo ON
+@echo OFF
 setlocal
 REM Author : Lal Thomas
 REM Date   : 2019-10-26
 
 set ViewerPath="E:\PortableApps.com\PortableApps\IrfanViewPortable\IrfanViewPortable.exe"
 set filelist="%USERPROFILE%\Desktop\slideshow.txt"
-IF NOT EXIST %filelist% ( call :PREPARE )
+IF NOT EXIST %filelist% ( 
+	call :PREPARE 
+) ELSE ( 
+	echo Showing Pictures from %filelist%
+	echo ...
+)
 %ViewerPath% /closeslideshow /slideshow=%filelist% 
 REM del %filelist%
 endlocal
-pause
+REM pause
 exit /b 0
 
 :PREPARE
 set rootfolder="F:\box"
+echo Preparing List from %rootfolder%
+echo ...
 pushd %rootfolder%
-@echo ON
-for /f "tokens=*" %%f in ('dir /b /tA /OD /A:D') do ( call :GENFILELIST %%~dpnxf )
+for /f "tokens=*" %%f in ('dir /b /TA /OD /A:D') do ( call :GENFILELIST %%~dpnxf )
 exit /b 0
 popd
 
 
 :GENFILELIST
-set picfolderpath="%1\pictures"
-IF EXIST %picfolderpath% ( 
-	pushd %picfolderpath%
-	dir /s /b /tA /OD *.jpg >>%filelist%
+set folderpath="%1\pictures"
+REM set folderpath="%1\documents"
+
+IF EXIST %folderpath% ( 
+	pushd %folderpath%
+	dir /s /b /TA /OD *.jpg >>%filelist%
 	popd
 )
 exit /b 0
