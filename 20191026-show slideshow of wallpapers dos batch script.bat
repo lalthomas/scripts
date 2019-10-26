99dfbe81dd14a3052ef74a840fdb3dea08b59f12
lalthomas <lal.thomas.mail@gmail.com>
2019-10-26 12:26:24 +0530

    update the script

--- 20191026-show slideshow of wallpapers dos batch script.bat
+++ 20191026-show slideshow of wallpapers dos batch script.bat
@@ -1,18 +1,31 @@
+@echo ON
+setlocal
 REM Author : Lal Thomas
 REM Date   : 2019-10-26
 
-REM Find directories with wallpapers
-REM Display the list and ask for the option
-
-
-
-
 set ViewerPath="E:\PortableApps.com\PortableApps\IrfanViewPortable\IrfanViewPortable.exe"
+set filelist="%USERPROFILE%\Desktop\slideshow.txt"
+IF NOT EXIST %filelist% ( call :PREPARE )
+%ViewerPath% /closeslideshow /slideshow=%filelist% 
+REM del %filelist%
+endlocal
+pause
+exit /b 0
 
-REM set dirs
-REM dir /b /s wallpapers
-set Directory="F:\box\2017\pictures\wallpapers\nature"
+:PREPARE
+set rootfolder="F:\box"
+pushd %rootfolder%
+@echo ON
+for /f "tokens=*" %%f in ('dir /b /tA /OD /A:D') do ( call :GENFILELIST %%~dpnxf )
+exit /b 0
+popd
 
-set ViewerOption="/closeslideshow /slideshow=%Directory%"
 
-%ViewerPath% %ViewerOption%
+:GENFILELIST
+set picfolderpath="%1\pictures"
+IF EXIST %picfolderpath% ( 
+	pushd %picfolderpath%
+	dir /s /b /tA /OD *.jpg >>%filelist%
+	popd
+)
+exit /b 0
