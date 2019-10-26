fa10bde2eaac16485a5446f09ff000f081f664ba
lalthomas <lal.thomas.mail@gmail.com>
2019-10-26 13:14:22 +0530

    add default picture folder option

--- 20191026-show slideshow of wallpapers dos batch script.bat
+++ 20191026-show slideshow of wallpapers dos batch script.bat
@@ -18,22 +18,32 @@ REM pause
 exit /b 0
 
 :PREPARE
+REM set rootfolder="%USERPROFILE%\Pictures"
 set rootfolder="F:\box"
 echo Preparing List from %rootfolder%
 echo ...
 pushd %rootfolder%
+
+REM running on pictures
+REM echo ; List generated on %DATE% from  %rootfolder% >> %filelist%
+REM dir /s /b /TA /OD *.jpg >>%filelist%
+
+REM running on all folders
+REM echo ; List generated on %DATE% from  %rootfolder% >> %filelist%
 for /f "tokens=*" %%f in ('dir /b /TA /OD /A:D') do ( call :GENFILELIST %%~dpnxf )
+
 exit /b 0
 popd
 
 
 :GENFILELIST
-set folderpath="%1\pictures"
-REM set folderpath="%1\documents"
+REM set folderpath="%1\pictures"
+set folderpath="%1\documents"
 
 IF EXIST %folderpath% ( 
 	pushd %folderpath%
 	dir /s /b /TA /OD *.jpg >>%filelist%
 	popd
 )
+
 exit /b 0
