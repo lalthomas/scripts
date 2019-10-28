163c84d7b60738d7dcec46925ff63b02c26c598b
lalthomas <lal.thomas.mail@gmail.com>
2019-10-28 08:57:38 +0530

    update the script to on an interval of seven days

--- 20191025-run dropit.bat
+++ 20191025-run dropit.bat
@@ -1,10 +1,46 @@
 @echo OFF
 REM DropIt
 set dropitdir="E:\Portable App\dropit v8.2 portable"
+set "cdate=%date%"
+set "cday=%date:~0,2%"
+set "cmonth=%date:~3,2%"
+set "cyear=%date:~6,4%"
+set tdate=%cyear%%cmonth%%cday%
+
+
+REM find last run date
+pushd %APPDATA%\DropIt
+
+REM initial run
+if not exist log ( call :RUNDROPIT )
+
+set lastrundate=%tdate%
+for /f "tokens=1 delims=." %%a in ('dir /o-N /b .\log') do set "lastrundate=%%a"
+
+popd
+
+REM call the dropit only if it has been 7 days
+set /a change=%tdate%-%lastrundate%
+if /i %change% GTR 7 ( call :RUNDROPIT ) 
+
+exit /b 0
+
+:RUNDROPIT
 pushd %dropitdir%
 DropIt.exe -bin "F:\bin\*"
 DropIt.exe -bin "%USERPROFILE%\*"
 DropIt.exe -backup "%USERPROFILE%\*"
 DropIt.exe -backup "C:\cygwin64\home\*"
 popd
+
+REM log the run
+pushd %APPDATA%\DropIt
+if not exist log ( md log )
+echo >log\%cyear%%cmonth%%cday%.log
+popd
+
+
+exit /b 0
+
+
 
