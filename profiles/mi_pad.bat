a98a65d0b552c9e46900cf097b66d4f79932e2ff
lalthomas <lal.thomas.mail@gmail.com>
2019-11-10 17:52:38 +0530

    fix the path issues and set inside for loop

--- profiles/mi_pad.bat
+++ profiles/mi_pad.bat
@@ -1,4 +1,5 @@
-@echo ON
+setlocal
+@echo OFF
 REM Author : Lal Thomas
 
 echo Processing Mi Pad Files
@@ -7,24 +8,38 @@ pushd %1
 
 REM Process each date folders
 for /f "tokens=*" %%f in ('dir /a:D /b') do (
-				
 		pushd %%f
 		for /f "tokens=*" %%D in ('dir /b /s /a:D') do (
-			
-			REM this I am doing because I couldn't find string comparison operators
-			set folder=%%D	
-			REM 11 is the length of "DCIM\Camera" string
-			set camdir=%folder:~-11%
-			if /i "%camdir%"=="DCIM\Camera" ( call :dcim "%%~dpnxD" )
-			
+			call :compare "%%D"
 		)
 		popd
 	)
 popd
+endlocal
 exit /b 0
 
+:compare
+setlocal
+set pt=%*
+REM converting path string to string
+set dp="%pt%"
+ REM removing double quotes with single quotes
+ set "dp=%dp:""="%
+
+
+REM this I am doing because I couldn't find string comparison operators
+REM 11 is the length of "DCIM\Camera" string
+set camdir=%dp:~-11%
+if "%camdir%" == "DCIM\Camera" ( call :dcim %pt% )
+
+endlocal
+exit /b 0
+
+
 :dcim
+setlocal
 pushd %dropitdir%
 DropIt.exe -file_year_date_taken "%1\*.jpg"
 popd
+endlocal
 exit /b 0
