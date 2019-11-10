37506be81cd2cf427ebbaf98c7ce8a90ccd09afd
lalthomas <lal.thomas.mail@gmail.com>
2019-11-10 17:53:21 +0530

    add vivo_y21l profile

--- 20191025-run dropit.bat
+++ 20191025-run dropit.bat
@@ -1,4 +1,5 @@
-@echo ON
+setlocal
+@echo OFF
 REM DropIt
 set dropitdir="E:\Portable App\dropit v8.2 portable"
 set "cdate=%date%"
@@ -7,6 +8,11 @@ set "cmonth=%date:~3,2%"
 set "cyear=%date:~6,4%"
 set tdate=%cyear%%cmonth%%cday%
 
+REM comment after testing
+call :RUNDROPIT
+pause
+endlocal
+exit /b 0
 
 REM find last run date
 pushd %APPDATA%\DropIt
@@ -20,6 +26,7 @@ popd
 REM call the dropit only if it has been 7 days
 set /a change=%tdate%-%lastrundate%
 if /i %change% GTR 7 ( call :RUNDROPIT ) 
+endlocal
 exit /b 0
 
 :RUNDROPIT
@@ -31,6 +38,7 @@ for /f "tokens=*" %%E in ('dir /a:D /b') do (
 	
 	REM if %%E EQU nokia_1 ( call "%~dp0\profiles\nokia_1.bat" "%%~dpnxE" )
 	if %%E EQU mi_pad ( call "%~dp0\profiles\mi_pad.bat" "%%~dpnxE" )
+	REM if %%E EQU vivo_y21l ( call "%~dp0\profiles\vivo_y21l.bat" "%%~dpnxE" )
 	
 	cd ..
 )
@@ -48,8 +56,4 @@ if not exist log ( md log )
 echo >log\%cyear%%cmonth%%cday%.log
 popd
 
-
 exit /b 0
-
-
-
