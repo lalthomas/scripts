d5faaf6399015520e56b73b52cf098ca58538e65
lalthomas <lal.thomas.mail@gmail.com>
2019-11-23 20:57:51 +0530

    update for folder files and format

--- 20191026-show slideshow of wallpapers dos batch script.bat
+++ 20191026-show slideshow of wallpapers dos batch script.bat
@@ -2,11 +2,10 @@
 setlocal
 REM Author : Lal Thomas
 REM Date   : 2019-10-26
-
 set ViewerPath="E:\PortableApps.com\PortableApps\IrfanViewPortable\IrfanViewPortable.exe"
 set filelist="%USERPROFILE%\Desktop\slideshow.txt"
 IF NOT EXIST %filelist% ( 
-	call :PREPARE 
+	call :PREPARE %1
 ) ELSE ( 
 	echo Showing Pictures from %filelist%
 	echo ...
@@ -19,31 +18,25 @@ exit /b 0
 
 :PREPARE
 REM set rootfolder="%USERPROFILE%\Pictures"
-set rootfolder="F:\box"
-echo Preparing List from %rootfolder%
+set rootfolder="%~1"
+IF NOT DEFINED %rootfolder% ( set /p rootfolder="Enter Path :" )
+REM IF NOT EXIST "%rootfolder%" ( exit /b 1)
+echo Preparing list from %rootfolder%
 echo ...
 pushd %rootfolder%
-
-REM running on pictures
-REM echo ; List generated on %DATE% from  %rootfolder% >> %filelist%
-REM dir /s /b /TA /OD *.jpg >>%filelist%
-
-REM running on all folders
-REM echo ; List generated on %DATE% from  %rootfolder% >> %filelist%
-for /f "tokens=*" %%f in ('dir /b /TA /OD /A:D') do ( call :GENFILELIST %%~dpnxf )
-
-exit /b 0
+REM running on pictures from the  List generated from %rootfolder%]
+for /f "tokens=*" %%f  in ('dir /b /TA /OD *.jpg') do ( echo %%~dpnxf >>%filelist% )
+REM running on all folders from the generated list from %rootfolder%
+for /f "tokens=*" %%g in ('dir /b /TA /OD /A:D') do ( call :GENFILELIST "%%~dpnxg" )
 popd
+exit /b 0
 
 
 :GENFILELIST
-REM set folderpath="%1\pictures"
-set folderpath="%1\documents"
-
+set folderpath="%~1"
 IF EXIST %folderpath% ( 
 	pushd %folderpath%
 	dir /s /b /TA /OD *.jpg >>%filelist%
 	popd
 )
-
 exit /b 0
