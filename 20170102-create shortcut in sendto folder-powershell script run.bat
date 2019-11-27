2ec9edec66a5f0366c1a5ba4ebf81ef4e8a3576a
lalthomas <lal.thomas.mail@gmail.com>
2019-11-27 20:34:39 +0530

    update the script

--- 20170102-create shortcut in sendto folder-powershell script run.bat
+++ 20170102-create shortcut in sendto folder-powershell script run.bat
@@ -4,4 +4,18 @@ REM Creation Date : 2017-01-17
 REM Author : Lal Thomas 	
 REM Original File : D:\lab\20131027-scripts project\20170102-create shortcut in sendto folder-powershell script.ps1 	
 	
-powershell -STA -executionpolicy bypass -noexit -File ".\%~nx1" -filename "%~dpnx0" -linkname "test shortcut.lnk"
+setlocal
+%~d0
+cd %~p0
+call :createShortCut %1 %2 %3
+endlocal
+exit /b 0
+
+:createShortCut
+set scriptfile=".\20170102-create shortcut in sendto folder-powershell script.ps1"
+IF [%3] == [] (
+	powershell -STA -executionpolicy bypass -File %scriptfile% -filename %1 -linkname %2 
+) ELSE (
+	powershell -STA -executionpolicy bypass -File %scriptfile% -filename %1 -linkname %2 -switch %3
+)
+exit /b 0
