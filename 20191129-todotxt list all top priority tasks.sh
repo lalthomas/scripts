8c03fed0e20dc780c069dd70c2e5548900b11020
lalthomas <lal.thomas.mail@gmail.com>
2019-12-02 09:02:55 +0530

    upate the script

--- 20191129-todotxt list all top priority tasks.sh
+++ 20191129-todotxt list all top priority tasks.sh
@@ -1,2 +1,7 @@
-b terminal
-t lsp A
+@echo OFF
+REM Author : Lal Thomas
+REM Date : 2019-11-29
+start C:\cygwin64\bin\mintty.exe -h always -s 150,36 -p center -e /usr/bin/bash -login -i -c "t lsp C"
+start C:\cygwin64\bin\mintty.exe -h always -s 150,36 -p center -e /usr/bin/bash -login -i -c "t lsp B"
+start C:\cygwin64\bin\mintty.exe -h always -s 150,36 -p center -e /usr/bin/bash -login -i -c "t lsp A"
+start C:\cygwin64\bin\mintty.exe -p right /bin/bash -il
